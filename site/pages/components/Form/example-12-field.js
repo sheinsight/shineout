12db92608cddba181d1e07d9d97944521d3b52e5
lobos <lobos841@gmail.com>
2018-03-23 21:45:11 +0800

    add color example

--- site/pages/components/Form/example-12-field.js
+++ site/pages/components/Form/example-12-field.js
@@ -3,8 +3,9 @@
  *    -- 支持 value 和 onChange 的自定义组件可以放在 Form.Field 中。
  * en - Field
  */
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { Form } from 'shineout'
+import { SketchPicker } from 'react-color'
 
 const rules = {
   email: [
@@ -28,6 +29,60 @@ function Input({ value = '', error, ...props }) {
   return <input style={style} value={value} {...props} />
 }
 
+const colorStyle = {
+  outer: {
+    position: 'relative',
+  },
+  handle: {
+    width: 16,
+    height: 16,
+    border: '4px solid #fff',
+    display: 'block',
+    boxSizing: 'content-box',
+    boxShadow: '0 0 1px 0 rgba(0, 0, 0, 0.4)',
+  },
+  picker: {
+    position: 'absolute',
+    left: 0,
+    top: '100%',
+    marginTop: 4,
+    zIndex: 1000,
+  },
+}
+
+/* eslint-disable */
+class ColorPicker extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = { showColor: false }
+  }
+
+  handleChange = (color) => {
+    this.props.onChange(color.hex)
+    this.setState({ showColor: false })
+  }
+
+  handleClick = () => { this.setState({ showColor: true }) }
+
+  render() {
+    const { value } = this.props
+    return (
+      <div style={colorStyle.outer}>
+        <a style={{ ...colorStyle.handle, background: value }}
+          onClick={this.handleClick}
+          href="javascript:;" />
+        {
+          this.state.showColor &&
+          <div style={colorStyle.picker}>
+            <SketchPicker color={value} onChange={this.handleChange} />
+          </div>
+        }
+      </div>
+    )
+  }
+}
+/* eslint-enable */
+
 export default function () {
   return (
     <Form rules={rules} style={{ maxWidth: 500 }} onSubmit={d => console.log(d)}>
@@ -40,6 +95,12 @@ export default function () {
       <Form.Item required label="Password" tip="Use at least one letter, one numeral, and seven characters.">
         <Form.Field name="password">
           <Input type="password" />
+        </Form.Field>
+      </Form.Item>
+
+      <Form.Item label="Favorite color" tip="Choose your favorite color.">
+        <Form.Field name="color" defaultValue="#7ED321">
+          <ColorPicker />
         </Form.Field>
       </Form.Item>
 
