079e506a6ae518da70c8f8c09fd02b2a90cba193
lobos <lobos841@gmail.com>
2018-03-30 11:50:39 +0800

    Table Element group

--- site/pages/components/Table/example-04-column-group.js
+++ site/pages/components/Table/example-04-column-group.js
@@ -7,11 +7,24 @@ import { Table } from 'shineout'
 import { fetchSync } from 'doc/data/table'
 
 const data = fetchSync(4)
+const name = (
+  <span
+    style={{
+      background: 'red',
+      display: 'block',
+      lineHeight: '40px',
+      color: '#fff',
+    }}
+  >
+    Name
+  </span>
+)
+
 const columns = [
   { title: 'id', render: 'id', width: 50 },
-  { title: 'First Name', render: 'firstName', group: ['Name', 'True Name'] },
-  { title: 'Last Name', render: 'lastName', group: ['Name', 'True Name'] },
-  { title: 'Nick Name', render: () => 'nickname', group: ['Name'] },
+  { title: 'First Name', render: 'firstName', group: [name, 'True Name'] },
+  { title: 'Last Name', render: 'lastName', group: [name, 'True Name'] },
+  { title: 'Nick Name', render: () => 'nickname', group: [name] },
   { title: 'Country', render: 'country' },
   { title: 'Office', render: 'office', group: 'Other' },
   { title: 'Position', render: 'position', group: 'Other' },
