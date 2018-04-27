f46a2c0217bbf4dff4f2fe392c6285e47f6a2e8b
lobos <lobos841@gmail.com>
2018-04-27 09:50:17 +0800

    build docs

--- 0.2.x/22.chunk.js
+++ 0.2.x/22.chunk.js
@@ -1,1 +1,1 @@
-webpackJsonp([22],{507:function(n,o){n.exports="# 更新日志\n\n## 0.2.x\n\n- [x] 增加 [Select](#/components/Select)\n  - [x] 单选，多选\n  - [x] 数据筛选\n  - [x] 创建条目\n  - [x] 性能优化\n- [x] 增加 [Modal](#/components/Modal)\n"}});
+webpackJsonp([22],{507:function(n,o){n.exports="# 更新日志\n\n## 0.2.x\n\n- [x] 增加 [Select](#/components/Select)\n  - [x] 单选，多选\n  - [x] 数据筛选\n  - [x] 创建条目\n  - [x] 性能优化\n- [x] 增加 [Card](#/components/Card)\n- [x] 增加 [Modal](#/components/Modal)\n"}});
