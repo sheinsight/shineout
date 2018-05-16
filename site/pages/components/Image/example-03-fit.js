6cfbe0db0778630533ecd9fc83fa275c883bd9fa
lobos <lobos841@gmail.com>
2018-05-16 09:44:32 +0800

    Image fit

--- site/pages/components/Image/example-03-fit.js
+++ site/pages/components/Image/example-03-fit.js
@@ -13,7 +13,7 @@ export default function () {
       {
         (['fill', 'center', 'fit', 'stretch']).map(fit => (
           <div key={fit} style={{ width: '25%', padding: 4, display: 'inline-block' }}>
-            <Image width="100%" height="70%" src={src} shape="thumbnail" fit={fit} />
+            <Image width="100%" height="75%" src={src} shape="thumbnail" fit={fit} />
             <div style={{ textAlign: 'center', paddingTop: 4 }}>{fit}</div>
           </div>
         ))
