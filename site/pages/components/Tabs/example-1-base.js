3485c5877fda42e0760aa6234892fe57123bf732
lobos <lobos841@gmail.com>
2018-06-20 13:22:35 +0800

    lorem

--- site/pages/components/Tabs/example-1-base.js
+++ site/pages/components/Tabs/example-1-base.js
@@ -4,6 +4,7 @@
  */
 import React from 'react'
 import { Tabs } from 'shineout'
+import lorem from 'doc/utils/faker/lorem'
 
 const panelStyle = { padding: '12px 0' }
 
@@ -11,13 +12,13 @@ export default function () {
   return (
     <Tabs defaultActive={1}>
       <Tabs.Panel style={panelStyle} tab="Home">
-        Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+        {lorem(5)}
       </Tabs.Panel>
       <Tabs.Panel style={panelStyle} tab="Profile">
-        Nulla est ullamco ut irure incididunt nulla Lorem Lorem minim irure officia enim reprehenderit. Magna duis labore cillum sint adipisicing exercitation ipsum. Nostrud ut anim non exercitation velit laboris fugiat cupidatat. Commodo esse dolore fugiat sint velit ullamco magna consequat voluptate minim amet aliquip ipsum aute laboris nisi. Labore labore veniam irure irure ipsum pariatur mollit magna in cupidatat dolore magna irure esse tempor ad mollit. Dolore commodo nulla minim amet ipsum officia consectetur amet ullamco voluptate nisi commodo ea sit eu.
+        {lorem(6)}
       </Tabs.Panel>
       <Tabs.Panel style={panelStyle} tab="Contact">
-        Sint sit mollit irure quis est nostrud cillum consequat Lorem esse do quis dolor esse fugiat sunt do. Eu ex commodo veniam Lorem aliquip laborum occaecat qui Lorem esse mollit dolore anim cupidatat. Deserunt officia id Lorem nostrud aute id commodo elit eiusmod enim irure amet eiusmod qui reprehenderit nostrud tempor. Fugiat ipsum excepteur in aliqua non et quis aliquip ad irure in labore cillum elit enim. Consequat aliquip incididunt ipsum et minim laborum laborum laborum et cillum labore. Deserunt adipisicing cillum id nulla minim nostrud labore eiusmod et amet. Laboris consequat consequat commodo non ut non aliquip reprehenderit nulla anim occaecat. Sunt sit ullamco reprehenderit irure ea ullamco Lorem aute nostrud magna.
+        {lorem(4)}
       </Tabs.Panel>
     </Tabs>
   )
--- site/pages/components/Tabs/example-2-card.js
+++ site/pages/components/Tabs/example-2-card.js
@@ -4,6 +4,7 @@
  */
 import React from 'react'
 import { Tabs } from 'shineout'
+import lorem from 'doc/utils/faker/lorem'
 import FontAwsome from '../Icon/FontAwesome'
 
 const panelStyle = { padding: 15 }
@@ -13,19 +14,19 @@ export default function () {
   return (
     <Tabs>
       <Tabs.Panel border="transparent" background="#ffe7ba" style={panelStyle} tab="Home">
-        Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+        {lorem(5)}
       </Tabs.Panel>
       <Tabs.Panel border="transparent" background="#ffc069" style={panelStyle} tab="Profile">
-        Nulla est ullamco ut irure incididunt nulla Lorem Lorem minim irure officia enim reprehenderit. Magna duis labore cillum sint adipisicing exercitation ipsum. Nostrud ut anim non exercitation velit laboris fugiat cupidatat. Commodo esse dolore fugiat sint velit ullamco magna consequat voluptate minim amet aliquip ipsum aute laboris nisi. Labore labore veniam irure irure ipsum pariatur mollit magna in cupidatat dolore magna irure esse tempor ad mollit. Dolore commodo nulla minim amet ipsum officia consectetur amet ullamco voluptate nisi commodo ea sit eu.
+        {lorem(5)}
       </Tabs.Panel>
       <Tabs.Panel border="transparent" color="#fff" background="#d46b08" style={panelStyle} tab={contact}>
-        Sint sit mollit irure quis est nostrud cillum consequat Lorem esse do quis dolor esse fugiat sunt do. Eu ex commodo veniam Lorem aliquip laborum occaecat qui Lorem esse mollit dolore anim cupidatat. Deserunt officia id Lorem nostrud aute id commodo elit eiusmod enim irure amet eiusmod qui reprehenderit nostrud tempor. Fugiat ipsum excepteur in aliqua non et quis aliquip ad irure in labore cillum elit enim. Consequat aliquip incididunt ipsum et minim laborum laborum laborum et cillum labore. Deserunt adipisicing cillum id nulla minim nostrud labore eiusmod et amet. Laboris consequat consequat commodo non ut non aliquip reprehenderit nulla anim occaecat. Sunt sit ullamco reprehenderit irure ea ullamco Lorem aute nostrud magna.
+        {lorem(5)}
       </Tabs.Panel>
       <Tabs.Panel border="transparent" color="#fff" background="#873800" style={panelStyle} tab="Setting">
-        Culpa dolor voluptate do laboris laboris irure reprehenderit id incididunt duis pariatur mollit aute magna pariatur consectetur. Eu veniam duis non ut dolor deserunt commodo et minim in quis laboris ipsum velit id veniam. Quis ut consectetur adipisicing officia excepteur non sit. Ut et elit aliquip labore Lorem enim eu. Ullamco mollit occaecat dolore ipsum id officia mollit qui esse anim eiusmod do sint minim consectetur qui.
+        {lorem(5)}
       </Tabs.Panel>
       <Tabs.Panel border="#b7eb8f" background="#f6ffed" style={panelStyle} tab="Message">
-        Culpa dolor voluptate do laboris laboris irure reprehenderit id incididunt duis pariatur mollit aute magna pariatur consectetur. Eu veniam duis non ut dolor deserunt commodo et minim in quis laboris ipsum velit id veniam. Quis ut consectetur adipisicing officia excepteur non sit. Ut et elit aliquip labore Lorem enim eu. Ullamco mollit occaecat dolore ipsum id officia mollit qui esse anim eiusmod do sint minim consectetur qui.
+        {lorem(5)}
       </Tabs.Panel>
     </Tabs>
   )
