0dfca46c63bfec0edcf2ea8f836e0c1f39ab97fc
lobos <lobos841@gmail.com>
2018-09-04 09:11:00 +0800

    build docs

--- 1.0.x/37.chunk.js
+++ 1.0.x/37.chunk.js
@@ -1,1 +1,1 @@
-webpackJsonp([37],{1165:function(n,e){n.exports="# 更新日志\n\n## 1.0.x\n\n#### 1.0.4\n- 撤销 1.0.3，移动到 componentDidMount 会导致 defaultValue 失效。\n\n#### 1.0.3\n- Form.inputable bind 移动到 componentDidMount 中（React 组件修改key时会先创建新组件，再移除旧组件）。\n\n#### 1.0.2\n- Select动态修改数据时，重置滚动条位置。\n\n#### 1.0.1\n- 修复 Table column render 函数丢失 index。"}});
+webpackJsonp([37],{1165:function(n,e){n.exports="# 更新日志\n\n## 1.0.x\n\n- Form.inputable bind 移动到 componentDidMount 中（React 组件修改key时会先创建新组件，再移除旧组件）。\n- Select动态修改数据时，重置滚动条位置。\n- 修复 Table column render 函数丢失 index。"}});
