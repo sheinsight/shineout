7884baf32494893ceb88d073b92dbab9dac6076f
兰轩轩 <lxx@bogon.host>
2019-01-22 11:55:30 +0800

    use jest on utils

--- test/utils.js
+++ test/utils.js
@@ -1,11 +1,3 @@
-import test from 'ava'
-import { range } from '../src/utils/numbers'
-
-export const repeat = (count, val) => range(count).map(() => val)
-
-export const createCases = (t, cases, result) => new Promise(async (resolve) => {
-  t.deepEqual(await Promise.all(cases), result)
-  resolve(true)
-})
-
-test('nothing', (t) => { t.pass() })
+export function isInstance(instance, constructor) {
+  return Object.getPrototypeOf(instance) === constructor.prototype
+}
