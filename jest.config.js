02bf2092f8ee367791071e11271d40327ffbc15d
Eastblue <xuanxuan_lan@126.com>
2019-01-29 14:14:21 +0800

    use enzyme, finish Button test

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@ module.exports = {
   setupFiles: [
     '<rootDir>/test/jest.init.js'
   ],
+  moduleNameMapper: {
+    '^shineout': '<rootDir>/src',
+    "\\.(css|less)$": "identity-obj-proxy"
+  },
+  globals: {
+    'SO_PREFIX': 'so',
+  },
 }
--- test/jest.init.js
+++ test/jest.init.js
@@ -1,3 +1,5 @@
-// enzyme
-
 import '@babel/polyfill'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
