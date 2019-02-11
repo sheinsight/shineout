12682eed1a7f5a6cff17f374868d045fd80f20b5
lobos <lobos841@gmail.com>
2019-02-11 15:10:16 +0800

    fixed example height

--- site/Components/CodeBlock.js
+++ site/Components/CodeBlock.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef } from 'react'
+import classnames from 'classnames'
 import Prism from 'prismjs'
 import 'prismjs/components/prism-jsx'
-import 'prismjs/themes/prism.css'
+import { exampleClass } from 'doc/styles'
 
 export default prop => {
   const { language = 'lang-jsx', onHighLight, value } = prop
@@ -16,7 +17,7 @@ export default prop => {
   }, [])
 
   return (
-    <pre ref={elRef} className={language || 'lang-jsx'}>
+    <pre ref={elRef} className={classnames(language || 'lang-jsx', exampleClass('pre'))}>
       <code>{value}</code>
     </pre>
   )
