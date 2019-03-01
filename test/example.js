5f367e0cb6777010ab02fa72801edd1b03f786a0
Eastblue <eastblue_lan@126.com>
2019-03-01 14:13:43 +0800

    Add snapshot tests for the example

---
+++ test/example.js
@@ -0,0 +1,24 @@
+import glob from 'glob'
+import React from 'react'
+import Render from 'react-test-renderer'
+
+function clearProps(tree) {
+  tree.props = {}
+  if (tree.children && tree.children.length > 0) {
+    tree.children.forEach(t => {
+      if (t.props) clearProps(t)
+    })
+  }
+}
+
+export default function exampleTest(module) {
+  const files = glob.sync(`site/pages/components/${module}/example-*.js`)
+  files.forEach(file => {
+    const Example = require(`../${file}`).default
+    test(`Snapshot test: ${module}[${file}]`, () => {
+      const wrapper = Render.create(<Example />).toJSON()
+      clearProps(wrapper)
+      expect(wrapper).toMatchSnapshot()
+    })
+  })
+}
