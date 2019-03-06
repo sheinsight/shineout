4538929c448b384c7de060e42a06e4d3620d8d22
nikomal <niko.wang@shein.com>
2019-03-06 10:07:53 +0800

    fixed decimal

--- src/Tooltip/Container.js
+++ src/Tooltip/Container.js
@@ -98,7 +98,7 @@ export default function(options) {
         default:
       }
 
-      return { left, top }
+      return { left: Math.round(left), top: Math.round(top) }
     }
 
     elementRef(el) {
