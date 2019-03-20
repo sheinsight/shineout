449cd35df2b679621ab0375822337b899aaeae21
nikomal <niko.wang@shein.com>
2019-03-20 17:11:07 +0800

    change breadcrumb style

--- src/Breadcrumb/index.js
+++ src/Breadcrumb/index.js
@@ -7,52 +7,47 @@ import { getKey } from '../utils/uid'
 
 class Breadcrumb extends React.PureComponent {
   render() {
-    const {
-      data, separator, keygen, renderItem,
-    } = this.props
+    const { data, separator, keygen, renderItem } = this.props
 
-    const className = classnames(
-      breadcrumbClass('_'),
-      this.props.className,
-    )
+    const className = classnames(breadcrumbClass('_'), this.props.className)
 
     return (
       <div className={className} style={this.props.style}>
-        {
-          data.map((d, index) => {
-            let item = d.title
-            if (!React.isValidElement(item)) {
-              if (d.onClick || d.url) {
-                item = <a onClick={d.onClick} href={d.url ? d.url : 'javascript:;'}>{d.icon} {d.title}</a>
-              } else {
-                item = <b>{d.title}</b>
-              }
+        {data.map((d, index) => {
+          let item = d.title
+          if (!React.isValidElement(item)) {
+            if (d.onClick || d.url) {
+              item = (
+                <a onClick={d.onClick} href={d.url ? d.url : 'javascript:;'}>
+                  {d.icon} {d.title}
+                </a>
+              )
+            } else {
+              item = <b>{d.title}</b>
             }
-            return (
-              <span key={keygen ? getKey(d, keygen, index) : index}>
-                { renderItem ? renderItem(d) : item }
-                { index !== data.length - 1 ? <span>{separator}</span> : null }
-              </span>
-            )
-          })
-        }
-      </div>)
+          }
+          return (
+            <span key={keygen ? getKey(d, keygen, index) : index}>
+              {renderItem ? renderItem(d) : item}
+              {index !== data.length - 1 ? <span className={breadcrumbClass('separator')}>{separator}</span> : null}
+            </span>
+          )
+        })}
+      </div>
+    )
   }
 }
 
 Breadcrumb.propTypes = {
   ...getProps(PropTypes),
-  dataSource: PropTypes.array,
+  data: PropTypes.array,
   renderItem: PropTypes.func,
-  separator: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.element,
-  ]),
+  separator: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
 }
 
 Breadcrumb.defaultProps = {
   ...defaultProps,
-  dataSource: [],
+  data: [],
   separator: '/',
 }
 
