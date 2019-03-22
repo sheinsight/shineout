8e2219f604b19f372f32c8a6c6b537f003f6ff9b
EastblueOkay <eastblue_lan@126.com>
2019-03-22 11:33:51 +0800

    go fix-table-scroll bug

---
+++ src/TreeSelect/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import inputable from '../Form/inputable'
+import { compose } from '../utils/func'
+import Datum from '../Datum'
+import inputBorder from '../hoc/inputBorder'
+import { selectClass } from '../styles'
+import Select from './Select'
+import filter from './filter'
+
+const limitWrap = Origin => props => {
+  // eslint-disable-next-line
+  const limit = props.multiple ? 0 : 1
+  return <Origin {...props} limit={limit} />
+}
+
+const exportSelect = compose(
+  inputable,
+  inputBorder({ className: selectClass('_'), tag: 'div' }),
+  limitWrap,
+  Datum.hoc({ bindProps: ['disabled', 'limit', 'format', 'prediction'] }),
+  filter
+)(Select)
+
+exportSelect.displayName = 'ShineoutSelect'
+
+export default exportSelect
