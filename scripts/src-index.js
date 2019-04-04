271d59f9ec20ddc9b83b34951d3d665130efb825
EastblueOkay <eastblue_lan@126.com>
2019-04-04 13:25:09 +0800

    add utils-expose

--- scripts/src-index.js
+++ scripts/src-index.js
@@ -9,7 +9,9 @@ const files = fs
 
 const line = `// Created by scripts/src-index.js.
 import './styles/normalize.less'
+import * as utils from './utils'
 
+export { utils }
 export { setLocale } from './locale'
 export { color, style } from './utils/expose'
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // Created by scripts/src-index.js.
 import './styles/normalize.less'
+import * as utils from './utils'
 
+export { utils }
 export { setLocale } from './locale'
 export { color, style } from './utils/expose'
 
