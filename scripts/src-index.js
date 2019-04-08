e3b23154c430ddc17bee2921f56d0db2261ab411
EastblueOkay <eastblue_lan@126.com>
2019-04-08 14:01:03 +0800

    add shineout.utils

--- scripts/src-index.js
+++ scripts/src-index.js
@@ -11,7 +11,7 @@ const line = `// Created by scripts/src-index.js.
 import './styles/normalize.less'
 import * as utils from './utils'
 
-export { utils }
+export default { utils }
 export { setLocale } from './locale'
 export { color, style } from './utils/expose'
 
