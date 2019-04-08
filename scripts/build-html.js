e25f8719d4e559f152c8b27673b7d4047d8b45b4
niko <niko.wang@shein.com>
2019-04-08 13:50:36 +0800

    react & search (#112)
    * react & search
    
    * docsearch

--- scripts/build-html.js
+++ scripts/build-html.js
@@ -5,11 +5,11 @@ const config = require('../config')
 
 const version = `${pkg.version.substr(0, pkg.version.lastIndexOf('.') + 1)}x`
 const cdn = 'https://unpkg.com'
+const dir = `gh-pages/${version}`
 
 async function buildHtml(lang) {
   const scripts = config.dev.scripts.map(p => cdn + p)
   const styles = config.dev.styles.map(p => cdn + p)
-  const dir = `gh-pages/${version}`
 
   Object.keys(config.webpack.entry).forEach(s => {
     scripts.push(`./${s}.js`)
@@ -23,5 +23,13 @@ async function buildHtml(lang) {
   fs.writeFileSync(`${dir}/${lang}.html`, html)
 }
 
+async function buildRedirect() {
+  const html = await ejs.renderFile(`./site/redirect.html`, {
+    version,
+  })
+  fs.writeFileSync(`${dir}/index.html`, html)
+}
+
 buildHtml('en')
 buildHtml('cn')
+buildRedirect()
