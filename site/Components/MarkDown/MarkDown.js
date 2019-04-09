7270f4b302a2511c50c2d816f5d0e9bb1f4b2e5f
nikomal <niko.wang@shein.com>
2019-04-09 14:20:58 +0800

    docs data

--- site/Components/MarkDown/MarkDown.js
+++ site/Components/MarkDown/MarkDown.js
@@ -12,6 +12,11 @@ import Table from '../Table'
 const codeReg = /^<code name="([\w|-]+)" /
 const exampleReg = /^<example name="([\w|-]+)"/
 
+const createId = (level, str) => {
+  if (level === 4) return getUidStr()
+  return (str || '').replace(/[\W|-]/g, '-')
+}
+
 export default function MarkDown({ onHeadingSetted, codes, examples, source }) {
   let [headings] = useState([])
   const [cache] = useState({})
@@ -42,7 +47,7 @@ export default function MarkDown({ onHeadingSetted, codes, examples, source }) {
 
     const text = locate('示例', 'Example')
 
-    const id = `heading-${getUidStr()}`
+    const id = `heading-example-h`
     appendHeading({
       id,
       level: 2,
@@ -91,7 +96,7 @@ export default function MarkDown({ onHeadingSetted, codes, examples, source }) {
     }
 
     if (!cache[key]) {
-      const id = `heading-${getUidStr()}`
+      const id = `heading-${createId(level, children[0])}`
       if (level === 2 || level === 3) {
         appendHeading({ id, level, children })
       }
--- site/pages/components/Table/example-01-base.js
+++ site/pages/components/Table/example-01-base.js
@@ -6,9 +6,61 @@
  */
 import React from 'react'
 import { Table } from 'shineout'
-import { fetchSync } from 'doc/data/user'
 
-const data = fetchSync(4)
+const data = [
+  {
+    id: 1,
+    firstName: 'Ephraim',
+    lastName: 'Wisozk',
+    position: 'Marketing Designer',
+    start: '2012-01-29',
+    time: '01:42',
+    salary: 115777,
+    country: 'Reunion',
+    office: 'Miami',
+    office5: 'Istanbul',
+    height: 113.74,
+  },
+  {
+    id: 2,
+    firstName: 'Osvaldo',
+    lastName: 'Beer',
+    position: 'Financial Controller',
+    start: '2007-09-04',
+    time: '03:26',
+    salary: 396093,
+    country: 'Syrian Arab Republic',
+    office: 'San Paulo',
+    office5: 'Shenzhen',
+    height: 82.13,
+  },
+  {
+    id: 3,
+    firstName: 'Dylan',
+    lastName: 'Ratke',
+    position: 'Development Lead',
+    start: '2009-10-16',
+    time: '01:45',
+    salary: 236064,
+    country: 'Peru',
+    office: 'Boston',
+    office5: 'Delhi',
+    height: 179.53,
+  },
+  {
+    id: 4,
+    firstName: 'Shaniya',
+    lastName: 'Jacobs',
+    position: 'Developer',
+    start: '2014-06-30',
+    time: '02:17',
+    salary: 338985,
+    country: 'Peru',
+    office: 'Chengdu',
+    office5: 'Dallas-Fort Worth',
+    height: 190.11,
+  },
+]
 
 const columns = [
   { title: 'id', render: 'id', width: 50 },
