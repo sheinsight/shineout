bfab80fb897b347c0c8bf0d8a886eb964a0c5c3e
EastblueOkay <eastblue_lan@126.com>
2019-04-15 10:26:55 +0800

    add Card

--- test/src/Card/Card.spec.js
+++ test/src/Card/Card.spec.js
@@ -6,21 +6,21 @@ import CardBase from '../../../site/pages/components/Card/example-1-base'
 import CardShallow from '../../../site/pages/components/Card/example-2-boxshadow'
 
 /* global SO_PREFIX */
-describe('Shineout[Base]', () => {
+describe('Card[Base]', () => {
   test('should render correct', () => {
     const wrapper = Render.create(<CardBase />).toJSON()
     expect(wrapper).toMatchSnapshot()
   })
 })
 
-describe('Shineout[Shallow]', () => {
+describe('Card[Shallow]', () => {
   test('should render correct', () => {
     const wrapper = Render.create(<CardShallow />).toJSON()
     expect(wrapper).toMatchSnapshot()
   })
 })
 
-describe('Shineout[Submit]', () => {
+describe('Card[Submit]', () => {
   test('should call form-onSubmit', done => {
     jest.useFakeTimers()
     const wrapper = mount(
@@ -63,15 +63,75 @@ describe('Card[collapsible]', () => {
     expect(wrapper.find('Hidable').prop('show')).toBeTruthy()
   })
   test('should collapsed controlled', () => {
+    class CardCollapsed extends React.Component {
+      constructor(props) {
+        super(props)
+        this.state = {
+          collapsed: false,
+        }
+      }
+
+      render() {
+        return (
+          <Card collapsible collapsed={this.state.collapsed}>
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <div style={{ height: 100 }} />
+            </Card.Body>
+          </Card>
+        )
+      }
+    }
+
     jest.useFakeTimers()
-    const wrapper = mount(
-      <Card collapsed={false}>
-        <Card.Header>Header</Card.Header>
-        <Card.Body>
-          <div style={{ height: 100 }} />
-        </Card.Body>
-      </Card>
-    )
+    const wrapper = mount(<CardCollapsed />)
+
     document.body.innerHTML += wrapper.html()
+    expect(wrapper.find(`div.${SO_PREFIX}-hidable-collapse`).hasClass(`${SO_PREFIX}-hidable-show`)).toBeTruthy()
+
+    wrapper.setState({
+      collapsed: true,
+    })
+    jest.runAllTimers()
+    expect(wrapper.html().indexOf(`${SO_PREFIX}-hidable-collapse`) > -1).toBeTruthy()
+  })
+})
+
+describe('Card[Accordion]', () => {
+  test('should always expand single', () => {
+    const wrapper = mount(
+      <Card.Accordion active={0}>
+        <Card>
+          <Card.Header>Header 1</Card.Header>
+          <Card.Body>Body</Card.Body>
+        </Card>
+        <Card>
+          <Card.Header>Header 2</Card.Header>
+          <Card.Body>Body</Card.Body>
+        </Card>
+        <Card>
+          <Card.Header>Header 3</Card.Header>
+          <Card.Body>Body</Card.Body>
+        </Card>
+      </Card.Accordion>
+    )
+    expect(
+      wrapper
+        .find('ShineoutCard')
+        .at(0)
+        .prop('collapsed')
+    ).toBeFalsy()
+    expect(
+      wrapper
+        .find('ShineoutCard')
+        .at(1)
+        .prop('collapsed')
+    ).toBeTruthy()
+    expect(
+      wrapper
+        .find('ShineoutCard')
+        .at(2)
+        .prop('collapsed')
+    ).toBeTruthy()
   })
 })
