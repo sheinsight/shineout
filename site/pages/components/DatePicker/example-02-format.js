50115d8a6c71ee36d37718c55b86307700d37daa
nikomal <niko.wang@shein.com>
2019-05-06 13:02:41 +0800

    format

--- site/pages/components/DatePicker/example-02-format.js
+++ site/pages/components/DatePicker/example-02-format.js
@@ -1,8 +1,8 @@
 /**
  * cn - 格式化
- *    -- 传入值可为 日期对象，时间戳，字符串，通过format 属性可以定义返回值的格式
+ *    -- 传入值可为 日期对象，时间戳，字符串，通过format 属性可以定义返回值的格式. <br /><br /> <b>注: 我们使用的格式化字符串(date-fns)和 moment.js 是不一致的, 如: <br /> moment.js:   YYYY-MM-DD hh:mm:ss <br /> date-fns:   yyyy-MM-dd HH:mm:ss</b> <br /><br /> 详细的请参照 <a href="#heading-3-Format">Format<a>
  * en - Format
- *    -- The format attribute defines the format of the return value
+ *    -- The format attribute defines the format of the return value.  <br /><br /> <b>tip: The format string we used (date-fns) and moment.js are inconsistent, such as: <br /> moment.js:   YYYY-MM-DD hh:mm:ss <br /> date-fns:   yyyy-MM-dd HH:mm:ss</b> <br /><br /> Please refer to the details <a href="#heading-3-Format">Format<a>
  */
 import React, { Component } from 'react'
 import { DatePicker, Input } from 'shineout'
