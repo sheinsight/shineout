758256a37d3cf98caa9ebeb474e019b15d5ee78f
EastblueOkay <eastblue_lan@126.com>
2019-07-09 15:12:37 +0800

    bugfix: Cascader absolute no-data option did not show

--- src/Cascader/Cascader.js
+++ src/Cascader/Cascader.js
@@ -172,6 +172,7 @@ class Cascader extends PureComponent {
         parentElement={this.element}
         data-id={this.selectId}
         zIndex={zIndex}
+        fixed="min"
       >
         {this.renderList()}
       </OptionList>
