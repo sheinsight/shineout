59a162bc4bb7974719962bde36fe4a3a9ad84502
lobos <lobos841@gmail.com>
2019-08-23 16:34:14 +0800

    version

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import './styles/normalize.less'
 import * as utils from './utils'
 
-export default { utils, version: '1.4.0' }
+export default { utils, version: '1.4.1-rc.2' }
 export { utils }
 export { setLocale } from './locale'
 export { color, style } from './utils/expose'
