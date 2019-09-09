47977e33471068b9d663d187018ed7aefc926c98
EastblueOkay <eastblue_lan@126.com>
2019-09-09 12:00:38 +0800

    bugfix: filtered Datu.Tree pathMap error (#325)

--- src/Datum/Tree.js
+++ src/Datum/Tree.js
@@ -14,9 +14,7 @@ export const CheckedMode = {
 
 export default class {
   constructor(options = {}) {
-    const {
-      data, value, keygen, mode, disabled,
-    } = options
+    const { data, value, keygen, mode, disabled } = options
 
     this.keygen = keygen
     this.mode = mode
@@ -45,6 +43,7 @@ export default class {
 
   getValue() {
     const value = []
+    const pathMap = this.getPathMap()
     this.valueMap.forEach((checked, id) => {
       switch (this.mode) {
         case CheckedMode.Full:
@@ -54,12 +53,12 @@ export default class {
           if (checked >= 1) value.push(id)
           break
         case CheckedMode.Child:
-          if (checked === 1 && this.pathMap.get(id).children.length === 0) value.push(id)
+          if (checked === 1 && pathMap.get(id).children.length === 0) value.push(id)
           break
         case CheckedMode.Shallow:
           if (checked === 1) {
             const parentChecked = (() => {
-              const { path } = this.pathMap.get(id)
+              const { path } = pathMap.get(id)
               const pid = path[path.length - 1]
               if (!pid) return false
               return this.valueMap.get(pid) === 1
@@ -80,15 +79,20 @@ export default class {
     if (update) update()
   }
 
+  setFilterPathMap(pathMap) {
+    this.storePathMap = pathMap
+  }
+
   set(id, checked, direction) {
     // self
     if (!this.isDisabled(id)) this.setValueMap(id, checked)
+    const pathMap = this.getPathMap()
 
-    const { path, children } = this.pathMap.get(id)
+    const { path, children } = pathMap.get(id)
 
     // children
     if (direction !== 'asc') {
-      children.forEach((cid) => {
+      children.forEach(cid => {
         this.set(cid, checked, 'desc')
       })
     }
@@ -97,7 +101,7 @@ export default class {
     if (direction !== 'desc' && path.length > 0) {
       const parentId = path[path.length - 1]
       let parentChecked = checked
-      this.pathMap.get(parentId).children.forEach((cid) => {
+      pathMap.get(parentId).children.forEach(cid => {
         if (parentChecked !== this.valueMap.get(cid)) {
           parentChecked = 2
         }
@@ -107,7 +111,7 @@ export default class {
   }
 
   isDisabled(id) {
-    const node = this.pathMap.get(id)
+    const node = this.getPathMap().get(id)
     if (node) return node.isDisabled
     return false
   }
@@ -116,12 +120,16 @@ export default class {
     return this.valueMap.get(id)
   }
 
+  getPathMap() {
+    return this.storePathMap || this.pathMap
+  }
+
   getDataById(id) {
     return this.dataMap.get(id)
   }
 
   getPath(id) {
-    return this.pathMap.get(id)
+    return this.getPathMap().get(id)
   }
 
   getChecked(id) {
@@ -133,23 +141,23 @@ export default class {
 
   getKey(data, id = '', index) {
     if (typeof this.keygen === 'function') return this.keygen(data, id)
-    else if (this.keygen) return data[this.keygen]
+    if (this.keygen) return data[this.keygen]
     return id + (id ? ',' : '') + index
   }
 
   initValue(ids, forceCheck) {
     if (!this.data || !this.value) return undefined
-
+    const pathMap = this.getPathMap()
     if (!ids) {
       ids = []
-      this.pathMap.forEach((val, id) => {
+      pathMap.forEach((val, id) => {
         if (val.path.length === 0) ids.push(id)
       })
     }
 
     let checked
-    ids.forEach((id) => {
-      const { children } = this.pathMap.get(id)
+    ids.forEach(id => {
+      const { children } = pathMap.get(id)
 
       if (forceCheck) {
         this.setValueMap(id, 1)
@@ -194,7 +202,11 @@ export default class {
         children = this.initData(d.children, [...path, id], isDisabled, indexPath)
       }
       this.pathMap.set(id, {
-        children, path, isDisabled, indexPath, index: i,
+        children,
+        path,
+        isDisabled,
+        indexPath,
+        index: i,
       })
     })
     return ids
