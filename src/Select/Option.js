96df456a7574dc9793b6d0ccb6751506f0ccfa02
EastblueOkay <eastblue_lan@126.com>
2019-09-18 17:54:12 +0800

    bugfix: Select acitve right mark☑️

--- src/Select/Option.js
+++ src/Select/Option.js
@@ -46,6 +46,11 @@ class Option extends PureComponent {
     return (
       <a tabIndex={-1} onClick={this.handleClick} onMouseEnter={this.handleEnter} className={className} title={title}>
         {result}
+        {isActive && (
+          <svg className={selectClass('check')} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
+            <path d="M913.017 237.02c-25.311-25.312-66.349-25.312-91.66 0l-412.475 412.474-206.237-206.237c-25.312-25.312-66.35-25.312-91.661 0s-25.312 66.35 0 91.66l252.067 252.067c0.729 0.73 1.439 1.402 2.134 2.029 25.434 23.257 64.913 22.585 89.527-2.029l458.303-458.303c25.313-25.312 25.313-66.35 0.001-91.661z" />
+          </svg>
+        )}
       </a>
     )
   }
