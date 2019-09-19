f663eceb97ab5eed34738e2a84b75abd51cc1f69
EastblueOkay <eastblue_lan@126.com>
2019-09-19 11:37:24 +0800

    Select (#348)
    * feat: TreeSelect compressed hover show all
    
    * bugfix: Select acitve right mark☑️
    
    * check to icon
    
    * add changelog
    
    * feat: TreeSelect onChange add path

--- test/src/Select/v_other.js
+++ test/src/Select/v_other.js
@@ -3,6 +3,7 @@ export function vAbsolute({ wrapper, data }) {
   wrapper.find(`.${SO_PREFIX}-select-inner`).simulate('click')
   const options = Array.prototype.slice.apply(document.getElementsByClassName(`${SO_PREFIX}-select-option`))
   options.forEach((option, index) => {
-    expect(option.innerHTML.trim()).toBe(data[index])
+    // console.log(option, option.innerText)
+    expect(option.innerHTML.startsWith(data[index])).toBeTruthy()
   })
 }
