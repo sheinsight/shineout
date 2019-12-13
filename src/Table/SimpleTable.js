053b13f16196bb5814b7b8f884c79327497acd8a
EastblueOkay <eastblue_lan@126.com>
2019-12-13 00:41:25 -0600

    bugfix: simple table colgroup error (#462)

--- src/Table/SimpleTable.js
+++ src/Table/SimpleTable.js
@@ -66,7 +66,9 @@ class SimpleTable extends PureComponent {
         colgroup.push(width)
       }
     }
-    this.setState({ colgroup })
+    setTimeout(() => {
+      this.setState({ colgroup })
+    })
   }
 
   handleScroll({ currentTarget }) {
