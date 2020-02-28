b403d6c4356f429ba1e4866c6f1f18bfdc181499
EastblueOkay <eastblue_lan@126.com>
2020-02-28 13:19:04 +0800

    Tabs (#543)
    * fix: isLink
    
    * add declear file

--- src/Table/index.d.ts
+++ src/Table/index.d.ts
@@ -3,6 +3,8 @@ import * as React from 'react';
 export interface TableProps {
     columns?: any[];
     data?: any[];
+    striped?: boolean;
+    style?: object;
     onRowSelect?: (...args: any[]) => any;
     datum?: Object;
     sorter?: (...args: any[]) => any;
@@ -13,6 +15,9 @@ export interface TableProps {
     defaultTreeExpandKeys?: any[];
     treeExpandKeys?: any[];
     onTreeExpand?: (...args: any[]) => any;
+    onScroll?: (x: number, y: number) => void;
+    rowClassName?: (record: any, index: number) => string;
+    rowsInView?: number;
 }
 
 export default class Table extends React.Component<TableProps, any> {
