83039ccd4eb7ad30bd096d73d6c8d520c84faffb
EastblueOkay <eastblue_lan@126.com>
2020-03-09 10:21:16 +0800

    add ts-build

--- scripts/build-es5.js
+++ scripts/build-es5.js
@@ -1,3 +1,5 @@
 const { execSync } = require('child_process')
+const bulidTS = require('./build-ts')
 
 execSync(`cross-env NODE_ENV=publish babel src --out-dir publish/css && cp -R publish/css/* publish/lib`)
+bulidTS()
