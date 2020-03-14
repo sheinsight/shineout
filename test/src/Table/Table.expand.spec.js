81ddcff5eb18f1366dad3916726cb18e26357562
EastblueOkay <eastblue_lan@126.com>
2020-03-14 13:12:17 +0800

    fix: popover in popover

--- test/src/Table/Table.expand.spec.js
+++ test/src/Table/Table.expand.spec.js
@@ -47,7 +47,8 @@ describe('Table[expand]', () => {
         expandKeys: [dataProp[i].id],
       })
       wrapper.update()
-      const expand = wrapper.find(`.${SO_PREFIX}-scroll table tbody tr`).at(i + 2)
+      const expand = wrapper.find(`.${SO_PREFIX}-scroll table tbody tr`).at(i + 1)
+      console.log(expand.debug())
       expect(expand.find('iframe')).toHaveLength(1)
     }
   })
