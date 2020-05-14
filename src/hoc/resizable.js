071c75604f3830f3b5d70a0f50cbd9f8184a8a8e
EastblueOkay <eastblue_lan@126.com>
2020-05-14 15:36:28 +0800

    add resizable hoc

--- src/hoc/resizable.js
+++ src/hoc/resizable.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
+import immer from 'immer'
 import { resizableClass } from '../styles'
 import { getUidStr } from '../utils/uid'
 import { curry } from '../utils/func'
@@ -16,7 +17,13 @@ export default curry(
 
       constructor(props) {
         super(props)
+        this.state = {
+          x: 0,
+          y: 0,
+        }
         this.resizableId = getUidStr()
+        this.handleMouseUp = this.handleMouseUp.bind(this)
+        this.handleMouseMove = this.handleMouseMove.bind(this)
       }
 
       componentDidMount() {
@@ -29,24 +36,73 @@ export default curry(
         }
       }
 
+      componentWillUnmount() {
+        if (this.handlers) {
+          this.handlers.forEach((action, handler) => handler.removeEventListener('mousedown', action))
+        }
+      }
+
+      getStyle() {
+        const { x, y } = this.state
+        if (!this.size) return undefined
+        return {
+          width: this.size.width + x,
+          height: this.size.height + y,
+        }
+      }
+
+      handleMouseMove(e) {
+        let x = e.movementX
+        let y = e.movementY
+        if (!this.active) return
+        this.setState(
+          immer(draft => {
+            x += draft.x
+            y += draft.y
+            if (this.active.indexOf('e') >= 0) draft.x = x
+            if (this.active.indexOf('s') >= 0) draft.y = y
+          })
+        )
+      }
+
+      handleMouseDown(dir) {
+        this.active = dir
+        document.addEventListener('mousemove', this.handleMouseMove)
+        document.addEventListener('mouseup', this.handleMouseUp)
+        document.addEventListener('mouseleave', this.handleMouseUp)
+      }
+
+      handleMouseUp() {
+        this.active = undefined
+        document.removeEventListener('mousemove', this.handleMouseMove)
+        document.removeEventListener('mouseup', this.handleMouseUp)
+        document.removeEventListener('mouseleave', this.handleMouseUp)
+      }
+
       appendHandler() {
         const { resizable } = this.props
         if (!resizable || this.appended) return
         this.appended = true
         this.el = document.querySelector(`.${resizableClass(this.resizableId)}`)
         if (!this.el) return
-        ;['top-left', 'top-right', 'bottom-left', 'bottom-right'].map(dir => {
-          const single = document.createElement('div')
-          single.className = resizableClass('handler', `handler-${dir}`)
-          this.el.appendChild(single)
+        this.size = this.el.getBoundingClientRect()
+        this.handlers = new Map()
+        ;['e', 's', 'se'].forEach(dir => {
+          const handler = document.createElement('div')
+          const action = this.handleMouseDown.bind(this, dir)
+          handler.className = resizableClass('handler', `handler-${dir}`)
+          handler.addEventListener('mousedown', action)
+          this.el.appendChild(handler)
+          this.handlers.set(handler, action)
         })
       }
 
       render() {
-        const { resizable, className, ...others } = this.props
+        const { resizable, className, style, ...others } = this.props
         if (!resizable) return <Origin {...this.props} />
+        const ms = Object.assign({}, style, this.getStyle())
         const mc = classnames(className, resizableClass('_', this.resizableId))
-        return <Origin {...others} className={mc} />
+        return <Origin {...others} style={ms} className={mc} />
       }
     }
 )
