5e9670388445f4932f2f0932f07284800c653d32
lsky walt <lsky_walt@163.com>
2020-06-03 14:37:32 +0800

    Feat/tab editable (#657)
    * feat: tab editable
    
    * fix: remove useless attr
    
    * Update Tag.js
    
    Co-authored-by: EastblueOkay <eastblue_lan@126.com>

--- src/Tag/Tag.js
+++ src/Tag/Tag.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { PureComponent } from '../component'
+import Input from './Input'
 import { getProps, defaultProps } from '../utils/proptypes'
 import Spin from '../Spin'
 import icons from '../icons'
 import { wrapSpan } from '../utils/dom/element'
-import { isPromise } from '../utils/is'
+import { isPromise, isFunc, isString, isEmpty } from '../utils/is'
 import { isDark } from '../utils/color'
 import { tagClass } from '../styles'
+
+const hideInput = 0
+const showInput = 1
 
 class Tag extends PureComponent {
   constructor(props) {
@@ -15,6 +19,8 @@ class Tag extends PureComponent {
 
     this.state = {
       dismiss: 0,
+      inputVisible: hideInput, // tag input status
+      value: null,
     }
 
     this.dismiss = this.dismiss.bind(this)
@@ -22,6 +28,16 @@ class Tag extends PureComponent {
     this.handleClose = this.handleClose.bind(this)
     this.renderClose = this.renderClose.bind(this)
     this.closeTag = this.closeTag.bind(this)
+    this.toggleInputVisible = this.toggleInputVisible.bind(this)
+    this.inputBlur = this.inputBlur.bind(this)
+    this.inputChange = this.inputChange.bind(this)
+  }
+
+  componentDidMount() {
+    const { children, onCompleted } = this.props
+    if (onCompleted && isString(children) && !isEmpty(children)) {
+      this.setState({ value: children })
+    }
   }
 
   closeTag() {
@@ -51,10 +67,34 @@ class Tag extends PureComponent {
     this.closeTag()
   }
 
+  inputBlur(value) {
+    const { onCompleted } = this.props
+    if (isFunc(onCompleted)) onCompleted(value)
+    this.setState({ inputVisible: hideInput })
+  }
+
+  inputChange(value) {
+    this.setState({ value })
+  }
+
+  toggleInputVisible() {
+    const { inputVisible, value } = this.state
+    const { onCompleted } = this.props
+    // if onCompleted is not null
+    if (onCompleted && !isEmpty(value))
+      this.setState({ inputVisible: inputVisible === hideInput ? showInput : hideInput })
+  }
+
   handleClick(e) {
     const { onClick, disabled } = this.props
     if (disabled) return
-    if (typeof onClick === 'function') onClick(e)
+
+    // toggle input visible
+    this.toggleInputVisible()
+
+    if (typeof onClick === 'function') {
+      onClick(e)
+    }
   }
 
   handleClose(e) {
@@ -83,10 +123,15 @@ class Tag extends PureComponent {
   }
 
   render() {
-    const { dismiss } = this.state
+    const { dismiss, inputVisible, value } = this.state
     if (dismiss === 2) return null
 
-    const { children, className, type, backgroundColor, onClose, disabled } = this.props
+    const { children, className, type, backgroundColor, onClose, disabled, onCompleted } = this.props
+
+    // if editable and input visible
+    if (onCompleted && inputVisible === showInput)
+      return <Input value={value} onBlur={this.inputBlur} onChange={this.inputChange} />
+
     const childrenParsed = wrapSpan(children)
     const { style } = this.props
 
@@ -124,6 +169,7 @@ Tag.propTypes = {
   children: PropTypes.any,
   onClose: PropTypes.oneOfType([PropTypes.func, PropTypes.bool]),
   backgroundColor: PropTypes.string,
+  onCompleted: PropTypes.func,
 }
 
 Tag.defaultProps = {
