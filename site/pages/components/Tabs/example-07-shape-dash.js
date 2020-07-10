aea9e23245de9c5aea5fe8febb4f95fc5b8bb219
EastblueOkay <eastblue_lan@126.com>
2020-07-10 15:34:55 +0800

    add tabs example

---
+++ site/pages/components/Tabs/example-07-shape-dash.js
@@ -0,0 +1,27 @@
+/**
+ * cn -
+ *    -- 设置 shape 为 'dash'，标签显示为短线条
+ * en -
+ *    -- dash tab type
+ */
+import React from 'react'
+import { Tabs } from 'shineout'
+import lorem from 'doc/utils/faker/lorem'
+
+const panelStyle = { padding: '12px 0' }
+
+export default function() {
+  return (
+    <Tabs shape="dash" defaultActive={1}>
+      <Tabs.Panel style={panelStyle} tab="Home">
+        {lorem(5)}
+      </Tabs.Panel>
+      <Tabs.Panel style={panelStyle} tab="Profile">
+        {lorem(6)}
+      </Tabs.Panel>
+      <Tabs.Panel style={panelStyle} tab="Contact">
+        {lorem(4)}
+      </Tabs.Panel>
+    </Tabs>
+  )
+}
