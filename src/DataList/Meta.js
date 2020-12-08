c8e29a196161bdc232e429d0b2533d422d0a8648
lsky_walt <lsky_walt@163.com>
2020-12-08 14:08:56 +0800

    fix: list import shineout

--- src/DataList/Meta.js
+++ src/DataList/Meta.js
@@ -1,7 +1,7 @@
 import React, { Component, isValidElement } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
-import { isFunc, isString } from 'shineout/utils/is'
+import { isFunc, isString } from '../utils/is'
 import { listClass } from '../styles'
 import Image from '../Image'
 
