24588b350b47cc39871ead005fb98e4781ec92b5
lsky丶walt <lsky_walt@163.com>
2020-12-08 14:13:22 +0800

    Fix/list import (#899)
    * fix: list import shineout
    
    * update: version and log

--- src/DataList/List.js
+++ src/DataList/List.js
@@ -1,7 +1,7 @@
 import React, { Component, isValidElement } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
-import { getLocale } from 'shineout/locale'
+import { getLocale } from '../locale'
 import { listClass } from '../styles'
 import { isFunc, isArray, isString } from '../utils/is'
 import { getKey } from '../utils/uid'
