a535111eac977cb349b7a8f2e060437f80210bb5
陈利浩 <leehaochen918@163.com>
2020-12-30 15:42:49 +0800

    edit

--- site/Components/Eggs.js
+++ site/Components/Eggs.js
@@ -2,15 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ready from 'shineout/utils/dom/ready'
 import styled from 'styled-components'
+import eggImg from '../images/egg_2021.png'
+import Image from '../../src/Image'
 
 const Mask = styled.div`
-  background: rgba(0, 0, 0, 0.4);
+  background: rgba(20, 23, 55, 0.6);
   position: fixed;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
-  z-index: 1100;
+  z-index: 110000;
 `
 
 const EGG_KEY = 'SHINEOUT_2021_EGG_HAPPY_NEW_YEAR'
@@ -33,12 +35,16 @@ export default class Eggs extends React.Component {
   }
 
   handleClose() {
-    localStorage.setItem(EGG_KEY, Date.now())
+    // localStorage.setItem(EGG_KEY, Date.now())
     this.setState({ visible: false })
   }
 
   renderContent() {
-    return <Mask onClick={this.handleClose}>Hello</Mask>
+    return (
+      <Mask onClick={this.handleClose}>
+        <Image src={eggImg} height="100%" placeholder={null} />
+      </Mask>
+    )
   }
 
   render() {
