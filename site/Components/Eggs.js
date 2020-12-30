2a445f8c14e9eeb20eed3e18b962c746688a3a17
陈利浩 <leehaochen918@163.com>
2020-12-30 16:34:24 +0800

    动画没做呢

--- site/Components/Eggs.js
+++ site/Components/Eggs.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import ready from 'shineout/utils/dom/ready'
 import styled from 'styled-components'
 import eggImg from '../images/egg_2021.png'
-import Image from '../../src/Image'
+import Icon from '../icons/Icon'
 
 const Mask = styled.div`
   background: rgba(20, 23, 55, 0.6);
@@ -13,6 +13,20 @@ const Mask = styled.div`
   bottom: 0;
   left: 0;
   z-index: 110000;
+  display: flex;
+  justify-content: center;
+`
+
+const Close = styled.div`
+  width: 26px;
+  height: 26px;
+  line-height: 26px;
+  text-align: center;
+  border-radius: 50%;
+  border: 1px solid;
+  color: #fff;
+  margin-top: 10%;
+  cursor: pointer;
 `
 
 const EGG_KEY = 'SHINEOUT_2021_EGG_HAPPY_NEW_YEAR'
@@ -27,11 +41,28 @@ ready(() => {
 export default class Eggs extends React.Component {
   state = {
     visible: !localStorage.getItem(EGG_KEY),
+    loaded: false,
   }
 
   constructor(props) {
     super(props)
     this.handleClose = this.handleClose.bind(this)
+    this.loadImg = this.loadImg.bind(this)
+  }
+
+  componentDidMount() {
+    if (this.state.visible) {
+      this.loadImg()
+      console.log('！！！')
+    }
+  }
+
+  loadImg() {
+    const image = new Image()
+    image.onload = () => {
+      this.setState({ loaded: true })
+    }
+    image.src = eggImg
   }
 
   handleClose() {
@@ -41,14 +72,17 @@ export default class Eggs extends React.Component {
 
   renderContent() {
     return (
-      <Mask onClick={this.handleClose}>
-        <Image src={eggImg} height="100%" placeholder={null} />
+      <Mask style={{}}>
+        <img src={eggImg} alt="元旦快乐～～～" height="100%" />
+        <Close onClick={this.handleClose}>
+          <Icon name="close" />
+        </Close>
       </Mask>
     )
   }
 
   render() {
-    const { visible } = this.state
+    const { visible, loaded } = this.state
     if (!visible) return null
     return ReactDOM.createPortal(this.renderContent(), container)
   }
