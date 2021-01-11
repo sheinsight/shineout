7667a48dc21f799bd006cc2a63557e4c0bc3d174
EastblueOkay <eastblue_lan@126.com>
2021-01-11 22:50:53 +0800

    update example

--- site/pages/components/Cascader/example-06-filter.js
+++ site/pages/components/Cascader/example-06-filter.js
@@ -1,8 +1,10 @@
 /**
  * cn - 筛选数据
  *    -- onFilter 可用于数据过滤，不返回结果时，可实现服务端过滤；返回函数时，用于前端过滤。
+ *    -- 单选状态下支持
  * en - Filter
  *    -- onFilter can be used for data filtering, for server-side filtering when no results are returned, and for front-end filtering when a function is returned.
+ *    -- Support in single selection state
  */
 import React from 'react'
 import { Cascader } from 'shineout'
