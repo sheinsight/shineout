1f63eea2b30b3a33a798551b4272183b985e1b26
lsky-walt <lsky_walt@163.com>
2021-01-11 16:10:51 +0800

    feat: table focus click

--- src/Table/select.js
+++ src/Table/select.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-
 import { addEventListener } from '../utils/dom/document'
 import { getParent } from '../utils/dom/element'
 import { isMacOS, isFirefox } from '../utils/is'
@@ -9,6 +7,7 @@ import { tableClass } from '../styles'
 
 const selectClass = tableClass('select')
 const trClass = tableClass('normal')
+const noSelection = tableClass('no-selection')
 
 // event combination
 function isEventCombination(event) {
@@ -16,67 +15,120 @@ function isEventCombination(event) {
   return (ismo && event.metaKey) || (!ismo && event.ctrlKey)
 }
 
+// 根据table tr层次   合并td
+function mergeDomsText(nodes) {
+  if (!nodes || nodes.length <= 0) return []
+  const trs = []
+  const res = []
+  nodes.forEach(node => {
+    const tr = getParent(node, `tr.${trClass}`)
+    let index = trs.indexOf(tr)
+    if (index === -1) {
+      trs.push(tr)
+      index = trs.length - 1
+    }
+    if (res[index]) {
+      res[index].push(node.innerText)
+    } else {
+      res[index] = [node.innerText]
+    }
+  })
+  return res
+}
+
+// format table text
+// 组合 text
+function formatTableText(arrs) {
+  if (!arrs || arrs.length <= 0) return ''
+  let txt = ''
+  arrs.forEach(value => {
+    if (typeof value === 'string') {
+      txt += `${value}\t`
+    } else {
+      txt += `${formatTableText(value)} \n`
+    }
+  })
+  return txt
+}
+
+// 生成 textarea，并且执行 copy
+function execCopyCommand(text) {
+  const textarea = document.createElement('textarea')
+  textarea.setAttribute('readonly', 'readonly')
+  textarea.value = text
+
+  // out window
+  textarea.style.position = 'fixed'
+  textarea.style.top = '-9999px'
+  textarea.style.left = '-9999px'
+
+  document.body.append(textarea)
+  textarea.select()
+  if (document.execCommand('copy')) {
+    document.execCommand('copy')
+  }
+  document.body.removeChild(textarea)
+}
+
 // remove all selectClass from dom
-function removeSelectClass(dom) {
+function removeSelectClass(dom, force = false) {
   if (dom) {
     dom.classList.remove(selectClass)
     return
   }
-  const nodes = document.querySelectorAll(`.${selectClass}`)
+  if (force) return
+  let nodes = document.querySelectorAll(`.${selectClass}`)
   if (nodes.length <= 0) return
   nodes.forEach(elem => {
     removeSelectClass(elem)
   })
+  nodes = document.querySelector(`.${noSelection}`)
+  if (nodes) nodes.classList.remove(`.${noSelection}`)
+}
+
+// add selection class
+function addSelectionClass(dom, className = selectClass) {
+  if (!dom) return
+  dom.classList.add(className)
+}
+
+function toggleNoSelection(flag) {
+  if (flag) {
+    document.body.classList.add(noSelection)
+    return
+  }
+  document.body.classList.remove(noSelection)
 }
 
 // handle document click
 function docClick(event) {
-  if (getParent(event.target, '.so-table-normal') && isEventCombination(event)) return
-  // console.log(event.target)
+  if (
+    (getParent(event.target, `.${tableClass('simple-body')}`) || getParent(event.target, `.${tableClass('body')}`)) &&
+    isEventCombination(event)
+  )
+    return
   removeSelectClass()
+  toggleNoSelection()
 }
 
-// handle click
-function handleClick(event) {
-  const dom = getParent(event.target, 'td')
-  if (isEventCombination(event) && dom) {
-    if (dom.classList.contains(selectClass)) {
-      dom.classList.remove(selectClass)
-      return
-    }
-    dom.classList.add(selectClass)
-  }
+// 批量操作
+// bulk operation doms
+function bulkOperation(doms, start, end) {
+  if (!doms || doms.length <= 0 || start <= -1 || end <= -1) return
+  const arr = Array.prototype.slice.call(doms.childNodes, start, end + 1)
+  arr.forEach(dom => {
+    addSelectionClass(dom)
+  })
 }
 
-const cache = {}
+let cache = {}
+let move = false
+let prevDom = null
 
-// mouse down
-function handleMouseDown(event) {
-  event.persist()
-  console.log('event: ', event)
-
-  const td = getParent(event.target, 'td')
-  const rect = td.getBoundingClientRect()
+function bulkAddSelectionClass(td) {
   const tr = getParent(td, `tr.${trClass}`)
-  const xIndex = Array.prototype.indexOf.call(tr.childNodes, td)
-  const yIndex = Array.prototype.indexOf.call(tr.parentNode.childNodes, tr)
-  // cache = {
-  //   rect,
-  //   xIndex,
-  //   yIndex,
-  // }
-  cache.rect = rect
-  cache.xIndex = xIndex
-  cache.yIndex = yIndex
-}
-// mouse up
-function handleMouseUp(event) {
-  event.persist()
-  console.log('event up: ', event)
-  console.log(event.target.getBoundingClientRect())
 
-  const td = getParent(event.target, 'td')
-  const tr = getParent(td, `tr.${trClass}`)
+  if (!tr) return
 
   const trs = tr.parentNode.childNodes
   const xIndex = Array.prototype.indexOf.call(tr.childNodes, td)
@@ -84,12 +136,67 @@ function handleMouseUp(event) {
   let count = 0
   // xIndex - cache.xIndex
   while (count < trs.length) {
+    if (cache.yIndex <= count) {
+      bulkOperation(trs[count], cache.xIndex, xIndex)
+    }
     if (trs[count] === tr) {
       break
     }
-    count++
+    count += 1
   }
 }
+
+// mouse down
+function handleMouseDown(event) {
+  if (!isEventCombination(event)) return
+  toggleNoSelection(true)
+
+  // toggle move
+  move = true
+
+  const td = getParent(event.target, 'td')
+  const tr = getParent(td, `tr.${trClass}`)
+  if (!tr) return
+  const xIndex = Array.prototype.indexOf.call(tr.childNodes, td)
+  const yIndex = Array.prototype.indexOf.call(tr.parentNode.childNodes, tr)
+  cache.xIndex = xIndex
+  cache.yIndex = yIndex
+}
+// mouse up
+function handleMouseUp(event) {
+  if (!isEventCombination(event)) return
+
+  const td = getParent(event.target, 'td')
+
+  bulkAddSelectionClass(td)
+
+  // reset
+  cache = {}
+  move = false
+}
+
+function handleMouseMove(event) {
+  if (!move) return
+
+  const td = getParent(event.target, 'td')
+  if (prevDom === td) return
+  prevDom = td
+
+  // clear class name
+  removeSelectClass()
+
+  bulkAddSelectionClass(td)
+}
+
+function handleKeyDown(event) {
+  if (!isEventCombination(event) || event.keyCode !== 67) return
+
+  const texts = formatTableText(mergeDomsText(document.querySelectorAll(`.${selectClass}`)))
+  execCopyCommand(texts)
+}
+
+// ctrl + c
+addEventListener(document, 'keydown', handleKeyDown)
 
 export default Table =>
   class extends React.Component {
@@ -103,9 +210,9 @@ export default Table =>
       this.events = {}
 
       if (!this.isFirefox) {
-        this.events.onClick = handleClick
         this.events.onMouseDown = handleMouseDown
         this.events.onMouseUp = handleMouseUp
+        this.events.onMouseMove = handleMouseMove
       }
     }
 
