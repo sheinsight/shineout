4f3b27ec0055b98c171574a4cf78f32911d20715
EastblueOkay <eastblue_lan@126.com>
2021-01-28 10:31:45 +0800

    add common typescript declare and utils (#993)
    * add common ts
    
    * update alert ts declare
    
    * update divider ts declare
    
    * update cascader ts declare
    
    * update pick useage

--- src/Divider/index.d.ts
+++ src/Divider/index.d.ts
@@ -1,44 +1,40 @@
 import * as React from 'react'
+import { StandardProps } from '../@types/common'
 
 type ReactNode = React.ReactNode;
 
-declare class Divider extends React.Component<DividerProps, {}> { 
-  
-  render(): JSX.Element;
-}
-
-export interface DividerProps {
+export interface DividerProps extends StandardProps{
   
     /**
      * desc: Content, text or react component
+     * 
+     * 分割线中文字内容
+     * 
      * default: -
      */
     children?: ReactNode;
   
     /**
-     * desc: Extend className
-     * default: -
-     */
-    className?: string;
-  
-    /**
-     * desc: Container element style
-     * default: -
-     */
-    style?: React.CSSProperties;
-  
-    /**
      * desc: mode of divider
+     * 
+     * 分割线排布模式
+     * 
      * default: *horizontal*
      */
     mode?: 'horizontal' | 'vertical';
   
     /**
      * desc: The position of title inside divider
+     * 
+     * 水平分割线的文字排布位置
+     * 
      * default: *center*
      */
     orientation?: 'center' | 'left' | 'right';
   
 }
 
+
+
+declare class Divider extends React.Component<DividerProps, {}>  {}
 export default Divider
