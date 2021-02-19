96ee709307969298742f063956fa59e68d1e91c1
EastblueOkay <eastblue_lan@126.com>
2021-02-19 13:28:53 +0800

    update transfer declare

--- src/@types/common.d.ts
+++ src/@types/common.d.ts
@@ -72,7 +72,6 @@ export interface FormItemStandardProps<Value = any> {
      */
     onChange?: (value: Value) => void;
 }
-
 export interface ListItemStandardProps<Item = any, Value = any> {
     /**
      * Auxiliary method for generating key. When it is a function, use the return value of this function. When it is a string, use the data value corresponding to this string. For example, 'id' is the same thing as (d) => d.id.
@@ -109,7 +108,7 @@ export interface ListItemStandardProps<Item = any, Value = any> {
      *
      * default: (val, d) => val===format(d)
      */
-    prediction?: (value: Value, data: Item) => boolean;
+    prediction?: (value: Value extends any[] ? Value[0] : Value, data: Item) => boolean;
 }
 
 export interface StructDataStandardProps<Item = any> {
