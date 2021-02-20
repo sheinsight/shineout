4df1232b868139dff95ed5a6bd0b7b0c49caea53
Dod_Annie <936166900@qq.com>
2021-02-20 15:39:24 +0800

    update input/textarea/slider types

---
+++ src/Slider/index.d.ts
@@ -0,0 +1,103 @@
+import * as React from 'react';
+import { StandardProps, FormItemStandardProps, CommonProps } from '../@types/common'
+
+export interface SliderProps<Value> extends
+StandardProps, 
+FormItemStandardProps<Value>, 
+Pick<CommonProps, 'absolute' | 'clearable' | 'zIndex'> 
+  {
+
+  /**
+   * Automatically hides the current value and scale
+   * 
+   * 是否自动隐藏当前值和刻度
+   * 
+   * default: false
+   */
+  autoHide?: boolean;
+
+  /**
+   * Disable component
+   * 
+   * 是否禁用组件
+   * 
+   * default: false
+   */
+  disabled?: boolean;  
+
+  /**
+   * Format displayed scale. When it is false, the scale is not displayed.
+   * 
+   * 格式化显示刻度，为false时，不显示刻度
+   * 
+   * default: v => v
+   */
+  formatScale?: (value: Value) => string | boolean;
+
+  /**
+   * Format displayed current value. When it is false, the current value is not displayed.
+   * 
+   * 格式化显示当前值，为false时，不显示当前值
+   * 
+   * default: 
+   */
+  formatValue?: (value: Value) => string | boolean;
+
+  /**
+   * height. Only effect when vertical is true
+   * 
+   * 高度，仅在 vertical 为 true 情况下有效
+   * 
+   * default: 200
+   */
+  height?: number;
+
+  /**
+   * Drag over the maximum event
+   * 
+   * 拖动超过最大值事件
+   * 
+   * default: -
+   */
+  onIncrease?: (value: Value) => boolean;
+
+  /**
+   * Whether to display double slider
+   * 
+   * 是否显示双滑块
+   * 
+   * default: false
+   */
+  range?: boolean;
+
+  /**
+   * Value range. An array whose length is greater than 2.
+   * 
+   * 取值范围，长度 >= 2 的数组
+   * 
+   * default: [0, 100]
+   */
+  scale?: number[];
+
+  /**
+   * Step size. Must be greater than or equal to 0; When it is 0, only the value specified by scale can be selected.
+   * 
+   * 步长，必须大于等于0；为0时，只能选取 scale 指定的值
+   * 
+   * default: 1
+   */
+  step?: number;
+
+  /**
+   * Whether to be vertical
+   * 
+   * 是否垂直
+   * 
+   * default: false
+   */
+  vertical?: boolean;
+}
+
+declare class Slider<Value = any> extends React.Component<SliderProps<Value>, {}> {}
+
+export default Slider;
--- src/Textarea/index.d.ts
+++ src/Textarea/index.d.ts
@@ -1,120 +1,98 @@
 import * as React from 'react';
+import { StandardProps, RegularAttributes, FormItemStandardProps, CommonProps } from '../@types/common'
+
 type ReactNode = React.ReactNode;
 
-
-
-declare class Textarea extends React.Component<TextareaProps, {}> {
-
-  render(): JSX.Element;
-}
-
-export interface TextareaProps {
+export interface TextareaProps<Value> extends
+StandardProps, 
+FormItemStandardProps<Value>, 
+Pick<CommonProps, 'absolute' | 'clearable' | 'zIndex'> 
+{
 
   /**
    * Whether the height changes automatically with the content
+   * 
    * 高度是否随内容自动变化
+   * 
    * default: false
    */
   autosize?: boolean;
 
-  /**
-   * default value
-   * 默认值
-   * default: 
-   */
-  defaultValue?: string | number;
 
   /**
    * User input triggers onChange and to check interval, unit: ms.
+   * 
    * 用户输入触发 onChange 和校验间隔时间，单位 毫秒。
+   * 
    * default: 400
    */
   delay?: number;
 
   /**
    * Infomation
+   * 
    * 提示信息
+   * 
    * default: -
    */
   info?: ((value: string) => string) | number;
 
   /**
-   * The name that accesses data from Form
-   * Form 存取数据的名称
-   * default: none
-   */
-  name?: string;
-
-  /**
-   * The callback function for changing value
-   * 值改变回调函数
-   * default: 
-   */
-  onChange?: (value: string) => void;
-
-  /**
    * The callback function for enter key
+   * 
    * 回车键回调函数
+   * 
    * default: 
    */
   onEnterPress?: (value: string) => void;
 
   /**
-   * The same as the native placeholder tag.
-   * 同原生 input 标签的 placeholder
-   * default: 
-   */
-  placeholder?: string;
-
-  /**
    * The position where the message pops up
+   * 
    * 信息弹出位置
+   * 
    * default: 
    */
-  popover?: 'top-left' | 'top' | 'top-right' | 'bottom-left' | 'bottom' | 'bottom-right';
+  popover?: RegularAttributes.Position;
 
   /**
    * The minimum row height. Same as native textarea rows property.
+   * 
    * 最小行高，同原生 textarea rows 属性
+   * 
    * default: 4
    */
   rows?: number;
 
   /**
    * the maxHeight of the textarea, scroll bars appear after more than
+   * 
    * 输入框的最大高度, 超过之后会出现滚动条
+   * 
    * default: -
    */
   maxHeight?: number | string;
 
   /**
-   * Container element style
-   * 最外层扩展样式
-   * default: -
-   */
-  style?: React.CSSProperties;
-
-  /**
    * When trim is true, blank characters are automatically deleted when lose focus。
+   * 
    * trim 为 true 时，失去焦点时会自动删除空白字符。
+   * 
    * default: false
    */
   trim?: boolean;
 
   /**
    * support resize
+   * 
    * 是否可以伸缩高度
+   * 
    * default: false
    */
   resize?: boolean;
 
-  /**
-   * DefaultValue and value can be set at the same time and defaultValue will be overridden by value. In the Form, the value is taken over by the Form and lose efficacy.
-   * defaultValue 和 value 可以同时设置，defaultValue 会被value覆盖。在Form中，value会被表单接管，value无效
-   * default: 
-   */
-  value?: string | number;
-
 }
+
+declare class Textarea<Value = any> extends React.Component<TextareaProps<Value>, {}> {}
 
 export default Textarea;
