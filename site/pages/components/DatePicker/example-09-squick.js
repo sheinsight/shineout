22099b6e5a762a4acd0a6fabfb46fe644c520ba3
lsky丶walt <lsky_walt@163.com>
2021-03-10 17:10:48 +0800

    feat: opti datepicker doc (#1041)

--- site/pages/components/DatePicker/example-09-squick.js
+++ site/pages/components/DatePicker/example-09-squick.js
@@ -18,6 +18,7 @@ export default function() {
       range
       onChange={d => console.log(d)}
       type="datetime"
+      placeholder={['Start datetime', 'End datetime']}
       quickSelect={[
         {
           name: 'Next Week',
