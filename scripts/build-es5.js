f2167c162a45459f7fc82ea645cf6cb9fbe7fd3a
EastblueOkay <eastblue_lan@126.com>
2021-03-29 17:11:12 +0800

    fix: build ts to dist (#1063)
    * fix: build ts to dist
    
    * feat: input.number add displayName
    
    * fix: reset form value equal
    
    * changelog

--- scripts/build-es5.js
+++ scripts/build-es5.js
@@ -1,3 +1,4 @@
 const { execSync } = require('child_process')
 
+execSync(`cp -r src/@types publish/lib`)
 execSync(`cross-env NODE_ENV=publish babel src --out-dir publish/css && cp -R publish/css/* publish/lib`)
