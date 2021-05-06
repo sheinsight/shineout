e0790aadc5decd54e123f8e62018a1c5d99f0afa
leo <chenlihao@shein.com>
2021-05-06 16:23:23 +0800

    - update Pagination/index.d.ts - update Grid/index.d.ts - update Tag/index.d.ts - update Tooltip/index.d.ts

--- src/Grid/index.d.ts
+++ src/Grid/index.d.ts
@@ -1,50 +1,38 @@
-import * as React from 'react';
-type ReactNode = React.ReactNode;
+import * as React from 'react'
+import {StandardProps} from '../@types/common'
 
-
-
-declare class Grid extends React.Component<GridProps, {}> {
-
-  render(): JSX.Element;
-}
-
-export interface GridProps {
-
-  /**
-   * Extend className
-   * 扩展className
-   * default: none
-   */
-  className?: string;
+export interface GridProps extends StandardProps{
 
   /**
    * Spacing between grids
+   *
    * 栅格之间间距
+   *
    * default: none
    */
   gutter?: number;
 
   /**
    * Left offset percentage, 0 <= offset < 1
+   *
    * 左偏移百分比，0 <= offset < 1
+   *
    * default: 0
    */
   offset?: number;
 
-  /**
-   * Container element style
-   * 最外层扩展样式
-   * default: -
-   */
-  style?: React.CSSProperties;
 
   /**
    * Percentage of width, 0 < number <= 1
+   *
    * 宽度百分比，0 < number <= 1
+   *
    * default: 1
    */
   width?: number;
 
 }
 
-export default Grid;
+declare class Grid extends React.Component<GridProps, any> {}
+
+export default Grid
