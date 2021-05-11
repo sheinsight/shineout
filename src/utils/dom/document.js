0b4b9aa7aab8fa9a782aeb1d48b8775b85f55b44
leo <chenlihao@shein.com>
2021-05-11 17:48:12 +0800

    fix unit test

--- src/utils/dom/document.js
+++ src/utils/dom/document.js
@@ -1,19 +1,3 @@
-// export const docScroll = {
-//   get top() {
-//     return document.documentElement.scrollTop || document.body.scrollTop
-//   },
-//   get left() {
-//     return document.documentElement.scrollLeft || document.body.scrollLeft
-//   },
-//   set top(value) {
-//     document.documentElement.scrollTop = value
-//     document.body.scrollTop = value
-//   },
-//   set left(value) {
-//     document.documentElement.scrollLeft = value
-//     document.body.scrollLeft = value
-//   },
-// }
 
 export const docScroll = (function() {
   let top
@@ -70,14 +54,6 @@ export const docSize = (function() {
     },
   }
 })()
-// export const docSize = {
-//   get width() {
-//     return document.documentElement.clientWidth || document.body.clientWidth
-//   },
-//   get height() {
-//     return document.documentElement.clientHeight || document.body.clientHeight
-//   },
-// }
 
 export function addEventListener(target, eventType, cb, option) {
   if (target.addEventListener) {
