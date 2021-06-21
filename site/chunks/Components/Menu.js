62ffdfe536ff48ce4ade3296f7c45f9a9e4c7424
EastblueOkay <eastblue_lan@126.com>
2021-06-21 13:47:13 +0800

    docs: add parent-menu onclick

--- site/chunks/Components/Menu.js
+++ site/chunks/Components/Menu.js
@@ -32,6 +32,15 @@ const examples = [
     rawText: require('!raw-loader!doc/pages/components/Menu/example-1-link.js'),
   },
   {
+    name: '1-onClick',
+    title: locate(
+      '父菜单可点击 \n 为父菜单数据设置 onClick 可以使其在点击后触发 Menu 的 onClick',
+      'Parent clickable \n Set onClick for the parent menu data to trigger the onClick of the Menu after clicking'
+    ),
+    component: require('doc/pages/components/Menu/example-1-onClick.js').default,
+    rawText: require('!raw-loader!doc/pages/components/Menu/example-1-onClick.js'),
+  },
+  {
     name: '2-horizontal',
     title: locate(
       '水平布局 \n 设置 mode 为 "horizontal"，显示为水平布局（子菜单在右侧弹出）',
