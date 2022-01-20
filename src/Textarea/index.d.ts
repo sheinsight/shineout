db136f82c1c5efa16365458aa3138121d41ddc88
leo <chenlihao@shein.com>
2022-01-20 16:11:22 +0800

    修复 textarea 文档 和 ts 中缺少 disabled

--- src/Textarea/index.d.ts
+++ src/Textarea/index.d.ts
@@ -2,24 +2,24 @@ import * as React from 'react'
 import { StandardProps, RegularAttributes, FormItemStandardProps } from '../@types/common'
 
 export interface TextareaProps<Value> extends
-StandardProps, 
+StandardProps,
 FormItemStandardProps<Value>
   {
 
   /**
    * show border bottom
-   * 
+   *
    * 仅仅展示下边框
-   * 
+   *
    * default: false
    */
   underline?: boolean;
 
   /**
    * Whether the height changes automatically with the content
-   * 
+   *
    * 高度是否随内容自动变化
-   * 
+   *
    * default: false
    */
   autosize?: boolean;
@@ -27,75 +27,86 @@ FormItemStandardProps<Value>
 
   /**
    * User input triggers onChange and to check interval, unit: ms.
-   * 
+   *
    * 用户输入触发 onChange 和校验间隔时间，单位 毫秒。
-   * 
+   *
    * default: 400
    */
   delay?: number;
 
   /**
    * Infomation
-   * 
+   *
    * 提示信息
-   * 
+   *
    * default: -
    */
   info?: ((value: string) => string) | number;
 
   /**
    * The callback function for enter key
-   * 
+   *
    * 回车键回调函数
-   * 
-   * default: 
+   *
+   * default:
    */
   onEnterPress?: (value: Value) => void;
 
   /**
    * The position where the message pops up
-   * 
+   *
    * 信息弹出位置
-   * 
-   * default: 
+   *
+   * default:
    */
   popover?: RegularAttributes.Position;
 
   /**
    * The minimum row height. Same as native textarea rows property.
-   * 
+   *
    * 最小行高，同原生 textarea rows 属性
-   * 
+   *
    * default: 4
    */
   rows?: number;
 
   /**
    * the maxHeight of the textarea, scroll bars appear after more than
-   * 
+   *
    * 输入框的最大高度, 超过之后会出现滚动条
-   * 
+   *
    * default: -
    */
   maxHeight?: number | string;
 
   /**
    * When trim is true, blank characters are automatically deleted when lose focus。
-   * 
+   *
    * trim 为 true 时，失去焦点时会自动删除空白字符。
-   * 
+   *
    * default: false
    */
   trim?: boolean;
 
   /**
    * support resize
-   * 
+   *
    * 是否可以伸缩高度
-   * 
+   *
    * default: false
    */
   resize?: boolean;
+
+  /**
+   * disabled
+   *
+   * 禁用
+   *
+   * default: false
+   */
+  disabled?: boolean;
+
+
 
 }
 
