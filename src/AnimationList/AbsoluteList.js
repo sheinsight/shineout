e5150cca3b7110dbba75f453f4ce4d9970d8b366
leo <chenlihao@shein.com>
2022-01-26 17:43:49 +0800

    Revert "Revert "修复AbsoluteList、Scroll 组件bug""
    This reverts commit a5afa3581287d0f092af7c6b2a900a497b281ef9.

--- src/AnimationList/AbsoluteList.js
+++ src/AnimationList/AbsoluteList.js
@@ -62,8 +62,8 @@ export default function(List) {
     }
 
     componentDidUpdate(prevProps) {
-      if (shallowEqual(prevProps.value, this.props.value)) return
       if (!this.props.focus) this.ajustdoc = false
+      if (shallowEqual(prevProps.value, this.props.value)) return
       setTimeout(() => {
         this.forceUpdate()
       })
