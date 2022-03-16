684a811fd1478553646f3629670d9686718a5cd9
leo <chenlihao@shein.com>
2022-03-16 17:52:43 +0800

    feat: Dropdown 组件支持阿拉伯语

--- src/Dropdown/index.js
+++ src/Dropdown/index.js
@@ -159,15 +159,24 @@ class Dropdown extends PureComponent {
   }
 
   renderButton(placeholder) {
-    const { type, outline, size, disabled, isSub } = this.props
+    const { type, outline, size, disabled, isSub, position } = this.props
     const rtl = isRTL()
     const buttonClassName = dropdownClass('button', !placeholder && 'split-button', rtl && 'rtl')
     const spanClassName = dropdownClass('button-content')
     const caret = (
-      <span className={dropdownClass('caret', rtl && 'rtl')}>
+      <span key="caret" className={dropdownClass('caret', rtl && 'rtl')}>
         <Caret />
       </span>
     )
+    const childs = [
+      <span key="text" className={spanClassName}>
+        {placeholder}
+      </span>,
+      caret,
+    ]
+    if (['left-bottom', 'left-top'].includes(position)) {
+      childs.reverse()
+    }
 
     if (isSub) {
       return (
@@ -177,8 +186,7 @@ class Dropdown extends PureComponent {
           data-role="item"
           onClick={this.handleFocus}
         >
-          <span className={spanClassName}>{placeholder}</span>
-          {caret}
+          {childs}
         </a>
       )
     }
@@ -193,8 +201,7 @@ class Dropdown extends PureComponent {
         size={size}
         key="button"
       >
-        <span className={spanClassName}>{placeholder}</span>
-        {caret}
+        {childs}
       </Button>
     )
   }
@@ -260,7 +267,7 @@ class Dropdown extends PureComponent {
     const { show } = this.state
     const position = this.getPosition()
 
-    let wrapClassName = dropdownClass('_', position, show && 'show', { 'split-dropdown': !placeholder })
+    let wrapClassName = dropdownClass('_', position, show && 'show', { 'split-dropdown': !placeholder, rtl: isRTL() })
     if (className) wrapClassName += ` ${className}`
 
     return (
