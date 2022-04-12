2c6fd25033db56034d143e8de4aff7deefefcba1
leo <chenlihao@shein.com>
2022-04-12 17:17:09 +0800

    fix: clientWidth Ignore the decimal point 修复 table 计算 colgroup clientWidth 忽略的小数点 在高分辨率显示器下会存在精度问题

--- src/Table/SimpleTable.js
+++ src/Table/SimpleTable.js
@@ -79,7 +79,7 @@ class SimpleTable extends PureComponent {
     const { columns } = this.props
     const colgroup = []
     for (let i = 0, count = tds.length; i < count; i++) {
-      const width = tds[i].offsetWidth
+      const { width } = tds[i].getBoundingClientRect()
       const colSpan = parseInt(tds[i].getAttribute('colspan'), 10)
       if (colSpan > 1) {
         split(width, range(colSpan).map(j => columns[i + j].width)).forEach(w => colgroup.push(w))
