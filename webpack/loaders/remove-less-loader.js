b51978341d60cd6b8f2a9441766d55a0cd4f5e0d
利浩 <chenlihao@shein.com>
2022-04-14 16:56:45 +0800

    Fix no normalize.less (#1577)
    * fix removeLessloader support require
    
    * changelog-1.9.4

--- webpack/loaders/remove-less-loader.js
+++ webpack/loaders/remove-less-loader.js
@@ -2,11 +2,15 @@ module.exports = function(resource) {
   // import chasingDotsLess from './spin/chasing-dots.less';
   const nameLessReg = /import ([a-zA-Z]+) from '[a-zA-Z\/\.\-]+.less';/g
   const lessReg = /import '[a-zA-Z\/\.\-]+.less';/g
+  const lessReg2 = /require\('[a-zA-Z\/\.\-]+.less'\);/g
   if (nameLessReg.test(resource)) {
     resource = resource.replace(nameLessReg, 'var $1 = {};')
   }
   if (lessReg.test(resource)) {
     resource = resource.replace(lessReg, '')
+  }
+  if (lessReg2.test(resource)) {
+    resource = resource.replace(lessReg2, '')
   }
   return resource
 }
