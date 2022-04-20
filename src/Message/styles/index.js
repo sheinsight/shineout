e27cf898fc4636cce548f33456373dd31c6ba06c
leo <chenlihao@shein.com>
2022-04-20 19:58:36 +0800

    修复Message 在esm 打包时样式出现问题

--- src/Message/styles/index.js
+++ src/Message/styles/index.js
@@ -1,4 +1,6 @@
 import '../../styles/normalize.less'
+// rely Alert
+import '../../Alert/styles/alert.less'
 import messageLess from './message.less'
 import genaration from '../../utils/classname'
 
