04a1ba9c4679b25d0361e5561415bb7fc5a49dcd
颜宇浩 <yanyuhao@shein.com>
2022-05-25 14:27:26 +0800

    feat: 创建ts打包脚本

---
+++ scripts/build-tsx.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const path = require('path')
+const ejs = require('ejs')
+const pack = require('../package')
+
+const srcPath = path.resolve(__dirname, '../src')
+const rootPath = path.resolve(__dirname, '../src')
+const files = fs
+    .readdirSync(rootPath)
+    .filter(n => fs.lstatSync(path.resolve(rootPath, n)).isDirectory() && /^[A-Z]/.test(n))
+    .filter(v => v !== 'List' && v !== 'DataList')
+// const files = []
+// function buildTsx(src) {
+//     if (!fs.existsSync(src)) fs.mkdirSync(src, { recursive: true })
+
+//     fs.readdirSync(src)
+//         .filter(name => !(/\.js$/.test(name)))
+//         .forEach(name => {
+//             const dir = path.resolve(src, name)
+//             const has = fs.readdirSync(dir).includes('index.d.ts')
+
+//             if (!has) return
+//             files.push(name)
+//             const ts = path.resolve(dir, './index.d.ts')
+//             // console.log(ts)
+//         })
+// }
+
+// buildTsx(srcPath)
+
+const line = `// Created by scripts/src-index.js.
+import * as utils from './utils'
+
+export default { utils, version: '<%= version %>' }
+export { utils }
+export { setLocale } from './locale'
+export { color, style } from './utils/expose'
+export { default as config, setConfig, isRTL } from './config'
+
+export { default as LazyList } from './AnimationList/LazyList'
+export { default as List } from './DataList'
+<% files.forEach(function (name) { -%>
+import {<%= name %>Props } from './<%= name %>'
+export { default as <%= name %> } from './<%= name %>'
+<% }) -%>
+
+export namespace Shein {
+    <% files.forEach(function (name) { -%>
+        export type <%= name %> = <%= name %>Props
+    <% }) -%>   
+}
+`
+
+
+
+
+
+const text = ejs.render(line, { files, version: pack.version })
+fs.writeFileSync(`${rootPath}/index.ts`, text)
