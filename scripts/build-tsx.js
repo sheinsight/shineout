76ec8e736f87de8fef5773bcd3035fc68c746ceb
颜宇浩 <yanyuhao@shein.com>
2022-06-02 18:23:38 +0800

    feat: ts自动化构建脚本调整，以组件为单位，独立声明namespace

--- scripts/build-tsx.js
+++ scripts/build-tsx.js
@@ -3,61 +3,80 @@ const path = require('path')
 const ejs = require('ejs')
 const pack = require('../package')
 
-const NAMESPACE = 'Type'
+const NAMESPACE = 'TYPE'
 
 const rootPath = path.resolve(__dirname, '../src')
 
+const components = {}
+
+const sortProps = props => {
+  props.sort((a, b) => a.length - b.length)
+  return props
+}
+
+const getProps = (file, name, mainProps = true) => {
+  const expExtends = /(?<=export interface ).*(?= extends)/g
+  const expDefault = /(?<=export interface ).*(?= {)/g
+  const extendProps = file.match(expExtends)
+  const defaultProps = file.match(expDefault)?.filter(f => f.indexOf('extends') == -1)
+  const props = []
+  if (extendProps) props.push(...extendProps)
+  if (defaultProps) props.push(...defaultProps)
+  if (mainProps) {
+    return [...new Set(props)]?.filter(p => p.indexOf(`${name}Props`) > -1)
+  } else {
+    return [...new Set(props)]?.filter(p => p.indexOf(`${name}Props`) === -1)
+  }
+}
+
+const getTypes = (file, name) => {
+  const exp = /(?<=export type ).*(?= =)/g
+  return file.match(exp) || []
+}
+
+const getArgs = types => {
+  const exp = /(?<=<).*(?=>)/
+  const result = types.toString().match(exp)
+  return result ? `<${result[0].split('=')[0]}>` : ''
+}
+
+const getTypeName = types => {
+  return types.toString().split('<')[0]
+}
+
+const getOtherTypeName = (types, name) => {
+  return types.split(name)[1] || types
+}
+
+const getAsName = types => {
+  if (Array.isArray(types) && types.length > 0) {
+    return types.map(i => `${getTypeName(i)} as __${getTypeName(i)}`)
+  }
+  if (typeof types === 'string' && types) {
+    return `${types} as __${types}`
+  }
+  return ''
+}
+
 const files = fs
-    .readdirSync(rootPath)
-    .filter(n => fs.lstatSync(path.resolve(rootPath, n)).isDirectory() && /^[A-Z]/.test(n))
-    .filter(v => v !== 'List' && v !== 'DataList' && v !== 'Rule')
-    .filter(f => fs.readdirSync(path.resolve(rootPath, f)).includes('index.d.ts'))
-
-const Types = []
-const AllTypes = {}
-const TypesDefault = []
-const TypesDeclare = []
-const WhiteList = ['Rule', 'Rate']
-files
-    .forEach(f => {
-        if (fs.readdirSync(path.resolve(rootPath, f)).includes('index.d.ts')) {
-            const tsPath = path.resolve(rootPath, `${f}/index.d.ts`)
-            const tsFile = fs.readFileSync(tsPath).toString()
-            const expDefault = /(?<=export interface ).*(?={)/g
-            const expExtend = /(?<=export interface ).*(?= extends)/g
-            const expDeclare = /(?<=export declare interface ).*(?= extends)/g
-            const types = tsFile.match(expExtend)
-            const typesDefault = tsFile.match(expDefault)
-            const typesDeclare = tsFile.match(expDeclare)
-            if (types) {
-                Types.push(types[0])
-            }
-
-            if (typesDefault) {
-                TypesDefault.push(typesDefault[0])
-            }
-
-            if (typesDeclare) {
-                TypesDeclare.push(typesDeclare[0])
-            }
-
-            typesDefault?.forEach((t, i) => {
-                typesDefault[i] = t.indexOf('extends') > -1 ? t.split(' extends ')[0] : t
-            })
-            let allTypes = []
-            if (types) allTypes.push(...types)
-            if (typesDefault) allTypes.push(...typesDefault)
-            if (typesDeclare) allTypes.push(...typesDeclare)
-            allTypes = [...new Set(allTypes)]?.filter(i => i.indexOf('Props') > -1 || WhiteList.includes(f))
-            AllTypes[f] = {
-                name: f,
-                props: allTypes,
-                propNames: allTypes.map(i => i.indexOf('<') > -1 ? i.split('<')[0] : i)
-            }
-
-        }
-    })
-
+  .readdirSync(rootPath)
+  .filter(n => fs.lstatSync(path.resolve(rootPath, n)).isDirectory() && /^[A-Z]/.test(n))
+  .filter(v => v !== 'List' && v !== 'DataList')
+  .filter(f => fs.readdirSync(path.resolve(rootPath, f)).includes('index.d.ts'))
+files.forEach(f => {
+  if (fs.readdirSync(path.resolve(rootPath, f)).includes('index.d.ts')) {
+    const tsPath = path.resolve(rootPath, `${f}/index.d.ts`)
+    const tsFile = fs.readFileSync(tsPath).toString()
+    const props = getProps(tsFile, f, true)
+    const other = getProps(tsFile, f, false)
+    const types = getTypes(tsFile, f)
+    components[f] = {
+      props: sortProps(props),
+      other: sortProps(other),
+      types: sortProps(types),
+    }
+  }
+})
 const line = `// Created by scripts/src-index.js.
 import * as utils from './utils'
 
@@ -69,30 +88,39 @@ export { default as config, setConfig, isRTL } from './config'
 
 export { default as LazyList } from './AnimationList/LazyList'
 export { default as List } from './DataList'
-<% files.forEach(function (name) { -%>
-<% let Props = AllTypes[name].propNames %>
-import { <%= Props.toString() %> } from './<%= name %>'
-export { default as <%= name %> } from './<%= name %>'
 
-<% }) -%>
+<% for(let key in components){ -%>
+export { default as <%= key %> } from './<%= key %>'
+import { <%- getAsName(getTypeName(components[key].props)) -%><%- components[key].props.length>0 ? ',' :'' -%><%- getAsName(components[key].other) -%> } from './<%= key %>'
 
+<% } -%>
+    
 export namespace <%= NAMESPACE -%> {
+<% for(let key in components){ -%>
+    export namespace <%= key -%> {
+    <% if(components[key].props.length>0){ -%>
+    export type Props<%- getArgs(components[key].props) -%> = __<%= key -%>Props<%- getArgs(components[key].props) -%>
+    <% } -%>
+    <% if(components[key].other.length>0){ -%>
+    <% components[key].other.forEach( name =>{ -%>
 
-<% for(let key in AllTypes){ -%>
-<% const EXP = /(?<=<).*(?=>)/ -%>
-<% AllTypes[key].props.forEach( (name,index) => { -%>
-<% let TYPE = name.match(EXP) -%>
-<% TYPE = TYPE ? TYPE.toString().split('=')[0] : TYPE -%>
-    export type <%- name.split('Props')[0]+ (TYPE ? '<'+TYPE+'>' : '') %> = <%- name.split('<')[0]+(TYPE ? '<'+TYPE+'>' : '') %>
-
-<% }) -%>
+        export type <%- getOtherTypeName(getTypeName(name),key) -%><%- getArgs(name) -%> = __<%- getTypeName(name)+getArgs(name) -%>
+    <% }) -%>
+    <% } -%>
+        
+    }
 <% } -%>
 }
 `
 
-
-
-
-
-const text = ejs.render(line, { files, version: pack.version, Types, AllTypes, NAMESPACE })
+const text = ejs.render(line, {
+  files,
+  getArgs,
+  getAsName,
+  NAMESPACE,
+  components,
+  getTypeName,
+  getOtherTypeName,
+  version: pack.version,
+})
 fs.writeFileSync(`${rootPath}/index.ts`, text)
