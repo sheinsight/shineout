f49031ea762b5f55d939d2ee45227adc30d16c7e
颜宇浩 <yanyuhao@shein.com>
2022-06-21 15:02:34 +0800

    feat: Icon 组件 ts 示例

---
+++ site/pages/components/Icon/example-1-awesome.tsx
@@ -0,0 +1,23 @@
+/**
+ * cn - 基本用法
+ *    -- 引入一个在线地址（本示例为 font-awesome）创建一个新的 Icon 组件，在需要使用的地方引入。
+ * en - Base
+ *    -- Create a new compoennt with url, then use it anywhere.
+ */
+import React from 'react'
+import { Icon } from 'shineout'
+
+const url = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+const margin = { marginInlineEnd: 20 }
+const FontAwesome = Icon(url, 'FontAwesome', 'fa')
+
+const App: React.FC = () => (
+  <div>
+    <FontAwesome style={margin} name="home" />
+    <FontAwesome style={margin} name="info" type="info" />
+    <FontAwesome style={margin} name="close" type="danger" />
+    <FontAwesome style={margin} name="check" type="success" />
+  </div>
+)
+
+export default App
