02189ee0947beedf17e740faa70bb3ded4c501ff
颜宇浩 <yanyuhao@shein.com>
2022-06-27 17:42:14 +0800

    feat: Message 组件 ts 示例

---
+++ site/pages/components/Message/example-3-position.tsx
@@ -0,0 +1,38 @@
+/**
+ * cn - 弹出位置
+ *    -- 设置 positoin 参数，修改显示位置，可以实现消息提醒展示位置，可选值：top, middle, top-left, top-right, bottom-left, bottom-right。
+ * en - Notification
+ *    -- Set position property to specify the pop-up layer location, optional value: top, middle, top-left, top-right, bottom-left, bottom-right.
+ */
+import React, { useState } from 'react'
+import { Button, Message, Select, TYPE } from 'shineout'
+
+type MessageProps = TYPE.Message.Props
+type MessagePosition = MessageProps['position']
+const App: React.FC = () => {
+  const [position, setPosition] = useState<MessagePosition>('top-right')
+
+  const show = () => {
+    Message.info(<div style={{ width: 240 }}>some message.</div>, 3, {
+      position,
+      title: 'notify title',
+    })
+  }
+
+  return (
+    <div>
+      position:
+      <Select
+        keygen
+        width={200}
+        value={position}
+        onChange={setPosition}
+        style={{ margin: '0 20px' }}
+        data={['top', 'middle', 'top-left', 'top-right', 'bottom-left', 'bottom-right']}
+      />
+      <Button onClick={show}>Show message.</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ site/pages/components/Message/example-5-manual-close.tsx
@@ -0,0 +1,26 @@
+/**
+ * cn - 手动关闭
+ *    -- Message 会异步返回一个关闭函数，调用它来关闭当前 Messsage
+ * en - Close
+ *    -- Message return close func async
+ */
+import React from 'react'
+import { Button, Message, TYPE } from 'shineout'
+
+type MessageProps = TYPE.Message.Props
+type MessageClose = MessageProps['onClose']
+
+const App: React.FC = () => {
+  const msg: MessageClose = async () => {
+    const close: any = await Message.success(
+      <div>
+        I will always show until
+        <a onClick={() => close()}> manually closed</a>
+      </div>,
+      0
+    )
+  }
+  return <Button onClick={msg}>Manual Close</Button>
+}
+
+export default App
