a5100e71a137e64f430ee7cdab6896a58f1ed19a
颜宇浩 <yanyuhao@shein.com>
2022-06-28 14:01:16 +0800

    feat: Pagination 组件 ts 示例

---
+++ site/pages/components/Pagination/example-1-base.tsx
@@ -0,0 +1,12 @@
+/**
+ * cn - 基本用法
+ *    -- 最基本的使用
+ * en - Base
+ *    -- The basic usage
+ */
+import React from 'react'
+import { Pagination } from 'shineout'
+
+const App: React.FC = () => <Pagination defaultCurrent={10} total={1000} />
+
+export default App
---
+++ site/pages/components/Pagination/example-7-controlled.tsx
@@ -0,0 +1,47 @@
+/**
+ * cn - 受控组件
+ *    -- 同时设置 current 和 onChange 属性，可以作为受控组件使用
+ * en - Controlled
+ *    -- Set both the current and onChange properties for being used as a controlled component.
+ */
+import React, { useState } from 'react'
+import { Pagination, TYPE } from 'shineout'
+
+type PaginationProps = TYPE.Pagination.Props
+type PaginationText = TYPE.Pagination.TextParams
+type PaginationOnChange = PaginationProps['onChange']
+
+const info = ({ current }: { current: number }) => `Current page ${current}`
+
+const text: PaginationText = {
+  next: 'Next',
+  page: '/ page',
+  prev: 'Previous',
+}
+
+const App: React.FC = () => {
+  const [current, setCurrent] = useState(1)
+  const [pageSize, setPageSize] = useState(20)
+
+  const handleChange: PaginationOnChange = (c, p) => {
+    setCurrent(c)
+    setPageSize(p)
+  }
+
+  return (
+    <div>
+      <Pagination
+        text={text}
+        total={1000}
+        current={current}
+        pageSize={pageSize}
+        onChange={handleChange}
+        layout={['links', 'list']}
+      />
+      <br />
+      <Pagination current={current} onChange={handleChange} pageSize={pageSize} total={1000} layout={['links', info]} />
+    </div>
+  )
+}
+
+export default App
