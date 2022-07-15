e6f83a2a660f32581dd6a484ce2f0856a5ea91d3
颜宇浩 <yanyuhao@shein.com>
2022-07-15 14:57:46 +0800

    feat: Switch 组件 ts 示例

--- site/pages/components/Switch/t-example-3-content.js
+++ site/pages/components/Switch/t-example-3-content.js
@@ -11,7 +11,7 @@ import FontAwesome from '../Icon/FontAwesome'
 export default function() {
   return (
     <div>
-      <Switch defaultValue={true} content={['开', '关']} />
+      <Switch defaultValue content={['开', '关']} />
       <br />
       <Switch content={[<FontAwesome name="btc" />, <FontAwesome name="yen" />]} />
     </div>
