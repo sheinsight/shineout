c63e7b16c3191935911bf776c7a398c68c6767f4
leo <chenlihao@shein.com>
2022-07-20 11:11:39 +0800

    update snapshot

--- site/chunks/Components/TreeSelect.js
+++ site/chunks/Components/TreeSelect.js
@@ -16,6 +16,7 @@ const examples = [
   {
     name: '01-base',
     isTs: false,
+    isTest: false,
     title: locate(
       '基本用法 \n 基础的TreeSelect用法。',
       'Base \n Basic usage of TreeSelect.'
@@ -27,6 +28,7 @@ const examples = [
   {
     name: '02-mutiple',
     isTs: false,
+    isTest: false,
     title: locate(
       '多选 \n 通过设置 multiple 来实现多选。',
       'Multiple \n Set multiple to Multiple choices.'
@@ -38,6 +40,7 @@ const examples = [
   {
     name: '03-filter',
     isTs: false,
+    isTest: false,
     title: locate(
       '筛选 \n onFilter 返回函数时，使用这个函数做前端过滤。',
       'Filter \n OnFilter is a function to filter data.'
@@ -49,6 +52,7 @@ const examples = [
   {
     name: '03-h-advanced-filter',
     isTs: false,
+    isTest: false,
     title: locate(
       '高级筛选 \n 高级筛选模式下，可针对当前层级在筛选结果和原始数据间切换 \n 设置 onAdvancedFilter 属性开启高级筛选，仅支持前端筛选',
       'Advanced Filter \n In the advanced filter mode, you can switch between the filter results and the original data for the current level by pressing the button \n Set the onAdvancedFilter property to enable advanced filtering, only front-end filtering is supported'
@@ -60,6 +64,7 @@ const examples = [
   {
     name: '04-disabled',
     isTs: false,
+    isTest: false,
     title: locate(
       '禁用 \n 设置 disabled 禁用选项',
       'Disabled \n Set disabled to disabled item.'
@@ -71,6 +76,7 @@ const examples = [
   {
     name: '05-loader',
     isTs: false,
+    isTest: false,
     title: locate(
       '动态加载 \n 数据过大，需要动态加载时，可以设置 loader 函数，当展开未定义 children（undefined）的节点时，触发此函数。',
       'Lazy load \n Set the loader function to dynamic fetch data. This function is triggered when the undefined child node is expanded.'
@@ -82,6 +88,7 @@ const examples = [
   {
     name: '06-innertitle',
     isTs: false,
+    isTest: false,
     title: locate(
       '内嵌标题 \n 使用 innerTitle 展示内嵌标题',
       'inner title \n use innerTitle to display the inner title'
