056925c6789fec1ee9476c0be16acdaa521ea3bc
leo <chenlihao@shein.com>
2022-07-20 15:57:41 +0800

    恢复 tsx

--- webpack/config.common.js
+++ webpack/config.common.js
@@ -73,7 +73,7 @@ module.exports = function getCommon(config) {
 
     resolve: {
       alias: config.alias,
-      extensions: ['.js', '.json', '.jsx'],
+      extensions: ['.js', '.json', '.jsx', 'tsx'],
     },
 
     resolveLoader: {
