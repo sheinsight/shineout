6898eb4c8571409dddbc288bfc655c9a84cdc3f4
leo <chenlihao@shein.com>
2022-07-20 10:38:18 +0800

    不生成video

--- cypress/e2e/Upload/bug.upload.cy.ts
+++ cypress/e2e/Upload/bug.upload.cy.ts
@@ -2,7 +2,7 @@ describe('Upload Bug', () => {
   it('修复 Upload 删除错误后 tip 仍然不显示的问题(< 1.10.12)', () => {
     cy.visit('/cn/components/Upload?example=test-001-tip')
     cy.get('.so-upload').as('Upload')
-    // cy.get('@Upload').find('input[type="file"]').invoke('removeAttr', 'style')
+    cy.get('.so-form-tip').should('have.length', 1)
     cy.get('@Upload').find('input[type="file"]').attachFile('dd.gif')
     cy.get('@Upload').find('input[type="file"]').attachFile('bear.gif')
     cy.get('.so-form-tip').should('have.length', 0)
