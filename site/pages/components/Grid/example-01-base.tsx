704e81697dd2d6296c987483cd3a08f7f1f2b35e
KMS-Bismarck <50952954+KMS-Bismarck@users.noreply.github.com>
2022-07-25 10:25:15 +0800

    Example ts gap icon (#1672)
    * feat: Gap 组件 ts 示例
    
    * feat: Grid 组件 ts 示例
    
    * feat: Icon 组件 ts 示例
    
    * feat: 测试更新
    
    * feat: 测试用例更新
    
    * feat: TS 示例声明优化
    
    * update Chunks
    
    * feat: 修改 icon 位置
    
    Co-authored-by: 颜宇浩 <yanyuhao@shein.com>
    Co-authored-by: leo <chenlihao@shein.com>

---
+++ site/pages/components/Grid/example-01-base.tsx
@@ -0,0 +1,45 @@
+/**
+ * cn - 任意等分
+ *    -- Grid 的栅格体系是动态生成，可以实现任意等份
+ * en - Arbitrary
+ *    -- Grid system is dynamic generated and can be any number.
+ */
+import React, { useState } from 'react'
+import { Grid, Slider, TYPE } from 'shineout'
+
+type SliderProps = TYPE.Slider.Props<number>
+type SliderOnChange = SliderProps['onChange']
+
+const gridStyle: React.CSSProperties = { color: '#fff', paddingLeft: 8, background: '#3399ff' }
+const style: React.CSSProperties = { background: '#f2f2f2', marginBottom: 4, lineHeight: '30px' }
+
+const App: React.FC = () => {
+  const [count, setCount] = useState(5)
+
+  const handleCountChange: SliderOnChange = v => {
+    setCount(v)
+  }
+  return (
+    <div>
+      <Slider
+        step={0}
+        value={count}
+        formatValue={false}
+        onChange={handleCountChange}
+        scale={[1, 2, 3, 5, 8, 13, 21, 34, 55]}
+      />
+
+      <div style={{ height: 20 }} />
+
+      {Array.from({ length: count }).map((_n, i) => (
+        <div key={i} style={style}>
+          <Grid width={(i + 1) / count} style={gridStyle}>
+            {`${i + 1}/${count}`}
+          </Grid>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
