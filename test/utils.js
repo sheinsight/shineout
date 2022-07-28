af3505c7602c13fc93f8d62a7260ce1603d16159
颜宇浩 <yanyuhao@shein.com>
2022-07-28 15:38:43 +0800

    feat: 补全 CardGroup 测试用例，完善单元测试

--- test/utils.js
+++ test/utils.js
@@ -1,3 +1,7 @@
+import enzyme from 'enzyme'
+import { func } from 'prop-types'
+import React from 'react'
+
 export function isInstance(instance, constructor) {
   return Object.getPrototypeOf(instance) === constructor.prototype
 }
@@ -30,4 +34,20 @@ export function appendToDOM(html) {
   const dom = document.createElement('div')
   dom.innerHTML = html
   document.body.appendChild(dom)
+}
+
+export function baseTest(Component, selector, style = { color: 'blue' }, className = 'class-name-test') {
+  const contains = (a, b) => Object.entries(b).every(([k, v]) => a[k] === v)
+  const wrapper = enzyme.mount(<Component style={style} className={className} />)
+
+  expect(wrapper.exists(selector)).toBeTruthy()
+  expect(wrapper.find(selector || Component).hasClass(className)).toBe(true)
+  expect(contains(wrapper.find(selector).getDOMNode().style, style)).toBeTruthy()
+}
+
+export function childrenTest(Component, selector) {
+  const children = <div>Test Children</div>
+  const wrapper = enzyme.mount(<Component>{children}</Component>)
+  expect(wrapper.exists(selector)).toBeTruthy()
+  return expect(wrapper.find(selector).contains(children)).toBeTruthy()
 }
