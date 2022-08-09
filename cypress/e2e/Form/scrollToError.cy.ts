ebdb206cc497722948c864454adf450cfe484ae7
leo <chenlihao@shein.com>
2022-08-09 19:32:25 +0800

    修复 cypress 报错问题

--- cypress/e2e/Form/scrollToError.cy.ts
+++ cypress/e2e/Form/scrollToError.cy.ts
@@ -3,7 +3,9 @@ describe('Upload scrollToError', () => {
     cy.visit('/cn/components/Form?example=07-validate')
     cy.get('.so-button-primary').click()
     cy.get('.so-input-invalid').then((el)=>{
-      expect(Math.round(el[0].getBoundingClientRect().top)).eq(30)
+      setTimeout(()=>{
+        expect(Math.round(el[0].getBoundingClientRect().top)).eq(30)
+      }, 200)
     })
   })
 })
