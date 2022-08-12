14cfbc28e00c8b0dff40336e6bc241801502dfdb
颜宇浩 <yanyuhao@shein.com>
2022-08-12 10:40:32 +0800

    feat: Slider 测试用例、单元测试补全

--- test/src/Slider/Slider.spec.js
+++ test/src/Slider/Slider.spec.js
@@ -40,6 +40,14 @@ describe('Slider[Base-action]', () => {
     const result = wrapper.find(`.${SO_PREFIX}-slider-result`).text()
     expect(result).toBe('100')
   })
+
+  test('should change value bigger than max while drag', () => {
+    const onIncrease = jest.fn()
+    const wrapper = mount(<Slider onIncrease={onIncrease} scale={[0, 200]} />)
+    wrapper.find('Indicator').prop('onDrag')(1, 0)
+    wrapper.find('Indicator').prop('onDragEnd')()
+    expect(onIncrease).toBeCalled()
+  })
 })
 
 describe('Slider[Range]', () => {
@@ -55,6 +63,64 @@ describe('Slider[Range]', () => {
           .text()
       ).toBe(String(value))
     })
+  })
+
+  test('should render double indicate when step is 0', () => {
+    const width = 200
+    const clientX = 100
+    const range = [0, 100]
+    const getBoundingClientRect = jest.fn(() => ({
+      top: 0,
+      left: 0,
+      height: 6,
+      width,
+    }))
+    const handleChange = jest.fn()
+    const wrapper = mount(<Slider range={range} step={0} onChange={handleChange} />)
+    expect(wrapper.find(`.${SO_PREFIX}-slider-indicator`).length).toBe(2)
+    wrapper.find(`.${SO_PREFIX}-slider-inner`).getDOMNode().getBoundingClientRect = getBoundingClientRect
+    wrapper.find(`.${SO_PREFIX}-slider-inner`).simulate('click', { clientX, clientY: 0 })
+    expect(handleChange).toBeCalled()
+    expect(handleChange.mock.calls[0][0].toString()).toBe(
+      [0, (clientX / width) * range[1] >= range[1] / 2 ? range[1] : range[0]].toString()
+    )
+  })
+
+  test('should render double indicate range', () => {
+    const width = 200
+    const clientX = 50
+    const range = [0, 100]
+    const getBoundingClientRect = jest.fn(() => ({
+      top: 0,
+      left: 0,
+      height: 6,
+      width,
+    }))
+    const handleChange = jest.fn()
+    const wrapper = mount(<Slider range={range} onChange={handleChange} />)
+    expect(wrapper.find(`.${SO_PREFIX}-slider-indicator`).length).toBe(2)
+    wrapper.find(`.${SO_PREFIX}-slider-inner`).getDOMNode().getBoundingClientRect = getBoundingClientRect
+    wrapper.find(`.${SO_PREFIX}-slider-inner`).simulate('click', { clientX, clientY: 0 })
+    expect(handleChange).toBeCalled()
+    expect(handleChange.mock.calls[0][0].toString()).toBe([0, (clientX / width) * range[1]].toString())
+  })
+
+  test('should render double indicate single', () => {
+    const width = 200
+    const clientX = 50
+    const getBoundingClientRect = jest.fn(() => ({
+      top: 0,
+      left: 0,
+      height: 6,
+      width,
+    }))
+    const handleChange = jest.fn()
+    const wrapper = mount(<Slider onChange={handleChange} />)
+    expect(wrapper.find(`.${SO_PREFIX}-slider-indicator`).length).toBe(1)
+    wrapper.find(`.${SO_PREFIX}-slider-inner`).getDOMNode().getBoundingClientRect = getBoundingClientRect
+    wrapper.find(`.${SO_PREFIX}-slider-inner`).simulate('click', { clientX, clientY: 0 })
+    expect(handleChange).toBeCalled()
+    expect(handleChange.mock.calls[0][0].toString()).toBe(((clientX / width) * 100).toString())
   })
 })
 
@@ -82,6 +148,19 @@ describe('Slider[Format]', () => {
       .forEach((item, index) => {
         expect(item.find('span').text()).toBe(format(scales[index]))
       })
+  })
+
+  test('should render format value', () => {
+    const defaultValue = 120
+    const formatValue = v => `value:${v}`
+    const wrapper = mount(
+      <Slider
+        formatValue={formatValue}
+        defaultValue={defaultValue}
+        scale={[0, 60, 120, 180, 240, 300, 360, 420, 480, 540]}
+      />
+    )
+    expect(wrapper.find(`.${SO_PREFIX}-slider-result`).text()).toBe(formatValue(defaultValue))
   })
 })
 
@@ -131,5 +210,20 @@ describe('Slider[Vertical]', () => {
     wrapper.find('ShineoutSlider').forEach(slider => {
       expect(slider.find(`.${SO_PREFIX}-slider-inner .${SO_PREFIX}-slider-top`).length).toBe(1)
     })
+  })
+})
+
+describe('Slider[autoHide]', () => {
+  test('should set autoHide', () => {
+    const wrapper = mount(<Slider autoHide />)
+    expect(wrapper.find(`.${SO_PREFIX}-slider-show`).length).toBe(0)
+  })
+})
+
+describe('Slider[height]', () => {
+  test('should set height', () => {
+    const height = 700
+    const wrapper = mount(<Slider vertical height={height} />)
+    expect(wrapper.find(`.${SO_PREFIX}-slider`).getDOMNode().style.height).toBe(`${height}px`)
   })
 })
