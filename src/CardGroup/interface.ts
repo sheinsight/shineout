9732ef2011fc1925d94881227bb3fe63eb1d93af
leo <chenlihao@shein.com>
2022-10-27 16:05:49 +0800

    Feat: CardGroup ts 重构

---
+++ src/CardGroup/interface.ts
@@ -0,0 +1,5 @@
+import {GetCardGroupItemProps, CardGroupProps as _CardGroupProps} from './Props'
+
+export type GroupItemProps<T> = GetCardGroupItemProps<T>
+
+export type CardGroupProps = _CardGroupProps
