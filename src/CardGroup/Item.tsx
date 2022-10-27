71e526e8817436f2d7e0eb09796a95e9e17e17cb
利浩 <chenlihao@shein.com>
2022-10-27 18:47:45 +0800

    Feat: CardGroup ts 重构 (#1795)

--- src/CardGroup/Item.tsx
+++ src/CardGroup/Item.tsx
@@ -1,17 +1,18 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, ReactNode } from "react"
 import classname from 'classnames'
-import PropTypes from 'prop-types'
 import Checkbox from '../Checkbox/Checkbox'
 import { cardGroupClass } from './styles'
 import Lazyload from '../Lazyload'
+import { CardGroupItemProps } from './Props'
 
-class Item extends React.Component {
-  handleChange(value, _, checked) {
+class Item<T> extends React.Component<CardGroupItemProps<T>, {}> {
+
+  handleChange(value: T, _: any, checked: boolean) {
     const { onChange } = this.props
     if (onChange) onChange(checked, value)
   }
 
-  renderChildren(content) {
+  renderChildren(content: ReactNode) {
     const { placeholder, container } = this.props
     if (!placeholder) return content
     return (
@@ -39,17 +40,6 @@ class Item extends React.Component {
       </div>
     )
   }
-}
-
-Item.propTypes = {
-  style: PropTypes.object,
-  className: PropTypes.string,
-  container: PropTypes.object,
-  children: PropTypes.element,
-  placeholder: PropTypes.element,
-  onChange: PropTypes.func,
-  value: PropTypes.any,
-  checked: PropTypes.oneOfType([PropTypes.oneOf([true, false, 'indeterminate']), PropTypes.func]),
 }
 
 export default Item
---
+++ src/CardGroup/context.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import createReactContext from '../context'
+import {CardContextValueType, GetCardConsumerProps} from './Props'
+
+const context = createReactContext<CardContextValueType>({})
+
+// eslint-disable-next-line
+export const Provider = context.Provider
+
+export const consumer = <U, >(Origin: React.ComponentType<U>) => (props: GetCardConsumerProps<U>) => (
+  <context.Consumer>{(value: CardContextValueType) => <Origin {...props as U} {...value} />}</context.Consumer>
+)
