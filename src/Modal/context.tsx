c3cb343e0a7caf48b611cf6067a04439beea6137
颜宇浩 <yanyuhao@shein.com>
2023-01-04 20:57:50 +0800

    feat: 更新 context

--- src/Modal/context.tsx
+++ src/Modal/context.tsx
@@ -6,20 +6,6 @@ const context = createReactContext({})
 // eslint-disable-next-line
 export const Provider = context.Provider
 
-<<<<<<<< HEAD:src/Table/context.tsx
-interface AbsoluteProps {
-  absolute?: boolean
-}
-
-export type GetAbsoluteProps<P> = Omit<P, 'absolute'> & AbsoluteProps
-
-const consumer = <T, >(Origin: React.ComponentType<T>): React.FC<T> => (props:T & AbsoluteProps) => (
-  <context.Consumer>
-    {value => {
-      // eslint-disable-next-line react/prop-types
-      const mp = Object.assign({}, props, value && props.absolute === undefined && { absolute: true })
-      return <Origin {...mp as T} />
-========
 const consumer = <Props extends {}>(Origin: React.ComponentType<Props>) => (
   props: Props & { absolute: string; zIndex: number }
 ) => (
@@ -28,7 +14,6 @@ const consumer = <Props extends {}>(Origin: React.ComponentType<Props>) => (
       // eslint-disable-next-line react/prop-types
       const mp = Object.assign({}, props, value && props.absolute && props.zIndex === undefined && { zIndex: 1051 })
       return <Origin {...mp} />
->>>>>>>> feat-2.0:src/Modal/context.tsx
     }}
   </context.Consumer>
 )
