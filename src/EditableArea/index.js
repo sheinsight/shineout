a7030394c7752fac84a80dc988e9b39b502a460c
利浩 <chenlihao@shein.com>
2023-01-09 14:31:08 +0800

    fix: 修复 Input Textarea  EditableArea 当设置trim 的时候 form 输入后直接点击提交会因为del… (#1873)
    * fix: 修复 Input Textarea  EditableArea 当设置trim 的时候 form 输入后直接点击提交会因为delay 导致 提交的数据没有trim 的问题
    
    * 优化

--- src/EditableArea/index.js
+++ src/EditableArea/index.js
@@ -1,13 +1,11 @@
 import { compose } from '../utils/func'
 import delay from '../hoc/delay'
-import trim from '../hoc/trim'
 import inputable from '../Form/inputable'
 import Component from './EditableArea'
 
 const EditableArea = compose(
   inputable,
-  delay(400),
-  trim
+  delay(400)
 )(Component)
 
 EditableArea.displayName = 'ShineoutEditableArea'
