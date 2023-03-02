49da3aad28455e75f1c2a5e0800e1d51f8d372cc
颜宇浩 <yanyuhao@shein.com>
2023-03-02 10:10:09 +0800

    feat:package.json 更新

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,7 +3,7 @@ import * as utils from './utils'
 
 declare const __default: {
   utils: typeof utils,
-  version: '1.12.1'
+  version: '1.12.4-beta.3'
 }
 
 export default __default
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Created by scripts/src-index.js.
 import * as utils from './utils'
 
-export default { utils, version: '1.12.1' }
+export default { utils, version: '1.12.4-beta.3' }
 export { utils }
 export { setLocale } from './locale'
 export { color, style } from './utils/expose'
