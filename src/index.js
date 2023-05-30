558084080332bd15957840d3aacacfe294fd5a38
颜宇浩 <yanyuhao@shein.com>
2023-05-30 18:16:58 +0800

    feat: 版本信息

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import * as utils from './utils'
 
-export default { utils, version: '2.0.2' }
+export default { utils, version: '2.0.0-rc.19' }
 export { utils }
 export { setLocale } from './locale'
 export { color, style } from './utils/expose'
