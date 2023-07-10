197b95901c82ae81acd8b12862df9eb273f2e6f3
KMS-Bismarck <50952954+KMS-Bismarck@users.noreply.github.com>
2023-07-10 10:31:53 +0800

    feat: 2.0.3 package.json (#2024)
    Co-authored-by: 颜宇浩 <yanyuhao@shein.com>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import * as utils from './utils'
 
-export default { utils, version: '2.0.3-beta.5' }
+export default { utils, version: '2.0.3' }
 export { utils }
 export { setLocale } from './locale'
 export { color, style } from './utils/expose'
