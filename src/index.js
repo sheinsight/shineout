0b232f27f4f23ea30c9af155ed1e7f95a2cadf45
颜宇浩 <yanyuhao@shein.com>
2023-08-01 11:25:51 +0800

    feat: changelog

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import * as utils from './utils'
 
-export default { utils, version: '2.0.5-beta.2' }
+export default { utils, version: '2.0.5-beta.6' }
 export { utils }
 export { setLocale } from './locale'
 export { color, style } from './utils/expose'
