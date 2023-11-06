d2a65d6f7f7f4b6e5a7b2aef0ca5350a759abe09
利浩 <chenlihao@shein.com>
2023-11-06 20:49:45 +0800

    feat: 修复 Popover 中点击元素被销毁会导致 Popover 关闭的问题

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import * as utils from './utils'
 
-export default { utils, version: '2.0.10' }
+export default { utils, version: '2.0.11-rc.1' }
 export { utils }
 export { setLocale } from './locale'
 export { color, style } from './utils/expose'
