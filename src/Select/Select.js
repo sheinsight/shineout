fdb9572161ccbb2238f2797aa7fb191d5457f721
KMS-Bismarck <50952954+KMS-Bismarck@users.noreply.github.com>
2023-12-29 17:43:24 +0800

    fix: 1.x 修复 Select 退格会删除禁用项的问题 (#2081)
    * fix: 修复 Select 退格删除功能误删禁用项的问题
    
    * feat: 优化算法
    
    * feat: 优化逻辑
    
    * feat: changelog
    
    ---------
    
    Co-authored-by: 颜宇浩 <yanyuhao@shein.com>

--- src/Select/Select.js
+++ src/Select/Select.js
@@ -9,7 +9,7 @@ import Result from './Result'
 import OptionList from './OptionList'
 import OptionTree from './OptionTree'
 import BoxList from './BoxList'
-import { isObject } from '../utils/is'
+import { isObject, isFunc } from '../utils/is'
 import { docSize } from '../utils/dom/document'
 import { getParent } from '../utils/dom/element'
 import { isRTL } from '../config'
@@ -427,8 +427,17 @@ class Select extends PureComponent {
     e.preventDefault()
     const raws = Array.isArray(value) ? value : [value]
     const values = [...raws]
-    const last = values.pop()
-    datum.handleChange(values, datum.getDataByValue(data, last), false)
+    if (isFunc(datum.disabled)) {
+      // find last deleteable value
+      for (let i = values.length - 1; i >= 0; i--) {
+        const item = values[i]
+        if (!datum.disabled(datum.getDataByValue(data, item))) {
+          values.splice(i, 1)
+          datum.handleChange(values, datum.getDataByValue(data, item), false)
+          return
+        }
+      }
+    }
   }
 
   handleFilter(...args) {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Created by scripts/src-index.js.
 import * as utils from './utils'
 
-export default { utils, version: '1.12.20-beta.0' }
+export default { utils, version: '1.12.20-beta.1' }
 export { utils }
 export { setLocale } from './locale'
 export { color, style } from './utils/expose'
