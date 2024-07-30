6726879fac33c98e60c8069b84870e6675fdf1e7
KMS-Bismarck <50952954+KMS-Bismarck@users.noreply.github.com>
2024-07-30 14:40:42 +0800

    fix: 修复 Input.Group 失焦后事件触发异常的问题 (#2118)
    Co-authored-by: 颜宇浩 <yanyuhao@shein.com>

--- src/Input/Group.tsx
+++ src/Input/Group.tsx
@@ -2,12 +2,21 @@ import React, { Children, cloneElement } from 'react'
 import { InputGroupProps } from './Props'
 
 function Group(props: InputGroupProps) {
-  const { children, style, inputFocus, ...other } = props
+  const { children, style, inputFocus, onBlur, ...other } = props
+
   return Children.toArray(children).map((child: React.ReactElement, i) => {
+    const onChlidBlur = (e: React.FocusEvent<HTMLElement>) => {
+      const childBlurEvent = child.props.onBlur
+      if (childBlurEvent) {
+        childBlurEvent(e)
+      }
+      onBlur(e)
+    }
+
     if (typeof child === 'string') {
       return <span key={i}>{child}</span>
     }
-    return cloneElement(child, other)
+    return cloneElement(child, { ...other, onBlur: onChlidBlur })
   })
 }
 
