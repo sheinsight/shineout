e054d0acedb57d2cc39811ccf6050ee1107e7592
saint3347 <zhaishengjia@shein.com>
2025-07-09 19:47:18 +0800

    chore: modify build script

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Created by scripts/src-index.js.
 import * as utils from './utils'
 
-export default { utils, version: '1.12.28-beta.1' }
+export default { utils, version: '1.12.28-beta.2' }
 export { utils }
 export { setLocale } from './locale'
 export { color, style } from './utils/expose'
