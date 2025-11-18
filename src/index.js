656c082cecf6d14c9962e6f684f33081ef7896a8
Tom Zhai <zhaishengjia@shein.com>
2025-11-18 16:47:07 +0800

    chore: release 2.0.30

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import * as utils from './utils'
 
-export default { utils, version: '2.0.30-beta.6' }
+export default { utils, version: '2.0.30' }
 export { utils }
 export { setLocale } from './locale'
 export { color, style } from './utils/expose'
